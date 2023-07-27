9368a6f093a4682fcb5777d0b66c722bdeb60eff
Michael Mi <guocuimi@gmail.com>
2023-07-26 18:12:35 -0700

    refactor install-* scripts.

--- scripts/install-libtorch.sh
+++ scripts/install-libtorch.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
+set -e
 
-# download torchlib and put into libtorch folder
+# Determine script's parent directory
+PARENT_DIR=$(dirname "$0")/..
+
+# Check if libtorch directory already exists
+if [ -d "${PARENT_DIR}/libtorch" ]; then
+    echo "libtorch folder already exists. Exiting to prevent overwrite."
+    exit 1
+fi
+
+# Determine libtorch version to download
 if lspci | grep -i -e nvidia > /dev/null; then
     echo "GPU found, downloading libtorch 2.0 with cuda 11.8"
     LIBTORCH_URL="https://download.pytorch.org/libtorch/cu118/libtorch-cxx11-abi-shared-with-deps-2.0.1%2Bcu118.zip"
 else
-    echo "No GPU found, downloading libtorch 2.0 with cpu"
+    echo "No GPU found, downloading libtorch 2.0 with CPU support"
     LIBTORCH_URL="https://download.pytorch.org/libtorch/cpu/libtorch-cxx11-abi-shared-with-deps-1.7.1%2Bcpu.zip"
 fi
 
-if [ -d "libtorch" ]; then
-    echo "libtorch folder already exist"
-else 
-    LIBTORCH=libtorch.zip
-    wget -O $LIBTORCH $LIBTORCH_URL
-    unzip $LIBTORCH && rm $LIBTORCH
-fi
+# Download, unzip, and remove the zip file
+LIBTORCH=libtorch.zip
+wget -O $LIBTORCH $LIBTORCH_URL
+unzip $LIBTORCH && rm $LIBTORCH
--- scripts/install-vcpkg.sh
+++ scripts/install-vcpkg.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
-
-# Install Vcpkg into .vcpkg folder
-
 set -e
-cd $(dirname "$0")/..
-VCPKG_DIR="$(pwd)/.vcpkg"
+
+# Install Vcpkg into $(PARENT_DIR)/.vcpkg folder
+
+# Determine script's parent directory
+PARENT_DIR=$(dirname "$0")/..
+
+VCPKG_DIR="${PARENT_DIR}/.vcpkg"
 echo "VCPKG_DIR: $VCPKG_DIR"
 
 if [[ -d "$VCPKG_DIR" ]]; then
-    pushd "$VCPKG_DIR"
-    ./bootstrap-vcpkg.sh --disableMetrics
-    popd
+    echo ".vcpkg folder already exists."
 else
+    echo ".vcpkg folder does not exist. Cloning Vcpkg into $VCPKG_DIR"
     # install Vcpkg
-    pushd "$(pwd)"
-    git clone -b 2023.06.20 https://github.com/Microsoft/vcpkg.git .vcpkg
-    cd .vcpkg
-    ./bootstrap-vcpkg.sh --disableMetrics
-    popd
+    git clone -b 2023.06.20 https://github.com/Microsoft/vcpkg.git ${VCPKG_DIR}
 fi
 
-# Create binary cache folder
+# bootstrap Vcpkg
+${VCPKG_DIR}/bootstrap-vcpkg.sh --disableMetrics
+
+# Create binary cache folder if it does not exist
 VCPKG_CACHE_DIR=${VCPKG_DEFAULT_BINARY_CACHE:-$VCPKG_DIR/bincache}
 mkdir -p "$VCPKG_CACHE_DIR"
