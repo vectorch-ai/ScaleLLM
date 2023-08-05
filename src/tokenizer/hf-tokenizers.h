f99ecd17e2a111fcfc61a2603725d9bc70a7bc97
Michael Mi <guocuimi@gmail.com>
2023-08-04 23:10:30 -0700

    wrap tokenizer_from_pretrained

--- src/tokenizer/hf-tokenizers.h
+++ src/tokenizer/hf-tokenizers.h
@@ -7,31 +7,25 @@
 
 using TokenizerHandle = void*;
 
-TokenizerHandle tokenizers_new_from_str(const char* json, size_t len);
+TokenizerHandle tokenizer_from_file(const char* path);
+TokenizerHandle tokenizer_from_pretrained(const char* identifier);
 
-TokenizerHandle byte_level_bpe_tokenizers_new_from_str(const char* vocab,
-                                                       size_t vocab_len,
-                                                       const char* merges,
-                                                       size_t merges_len,
-                                                       const char* added_tokens,
-                                                       size_t added_tokens_len);
+void tokenizer_encode(TokenizerHandle handle,
+                      const char* data,
+                      size_t len,
+                      int add_special_token);
 
-void tokenizers_encode(TokenizerHandle handle,
-                       const char* data,
-                       size_t len,
-                       int add_special_token);
+void tokenizer_decode(TokenizerHandle handle,
+                      const uint32_t* data,
+                      size_t len,
+                      int skip_special_token);
 
-void tokenizers_decode(TokenizerHandle handle,
-                       const uint32_t* data,
-                       size_t len,
-                       int skip_special_token);
+void tokenizer_get_decode_str(TokenizerHandle handle,
+                              const char** data,
+                              size_t* len);
 
-void tokenizers_get_decode_str(TokenizerHandle handle,
-                               const char** data,
-                               size_t* len);
+void tokenizer_get_encode_ids(TokenizerHandle handle,
+                              const uint32_t** id_data,
+                              size_t* len);
 
-void tokenizers_get_encode_ids(TokenizerHandle handle,
-                               const uint32_t** id_data,
-                               size_t* len);
-
-void tokenizers_free(TokenizerHandle handle);
+void tokenizer_free(TokenizerHandle handle);
