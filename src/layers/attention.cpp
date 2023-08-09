2e0088d5c59dc0e9d70d6a2804fefda135a42ff8
Michael Mi <guocuimi@gmail.com>
2023-08-08 18:27:38 -0700

    simple code in attention with torch::einsum

--- src/layers/attention.cpp
+++ src/layers/attention.cpp
@@ -16,28 +16,15 @@ torch::Tensor SelfAttentionImpl::forward(
     float scale) const {
   const auto bsz = query.size(0);
   const auto seqlen = query.size(1);
-
-  // (bs, n_local_heads, seqlen, head_dim)
-  query = query.transpose(1, 2);
-  key = key.transpose(1, 2);
-  value = value.transpose(1, 2);
-
-  // [bs, n_local_heads, seqlen, head_dim] x [bs, n_local_heads, head_dim,
-  // cache_len + seqlen]
-  // => [bs, n_local_heads, seqlen, cache_len + seqlen]
-  auto scores = torch::matmul(query, key.transpose(2, 3)) * scale;
+  query = query * scale;
+  auto scores = torch::einsum("bqhd,bkhd->bhqk", {query, key});
   if (mask.defined()) {
-    // (bs, n_local_heads, seqlen, cache_len + seqlen)
     scores += mask;
   }
   // (bs, n_local_heads, seqlen, cache_len + seqlen)
-  scores = torch::softmax(scores.to(torch::kFloat), -1).type_as(query);
-  // (bs, n_local_heads, seqlen, cache_len + seqlen) x [bs, n_local_heads,
-  // cache_len + seqlen, head_dim]
-  // => [bs, n_local_heads, seqlen, head_dim]
-  auto output = torch::matmul(scores, value);
-  // (bs, seqlen, dim = n_local_heads X head_dim)
-  return output.transpose(1, 2).contiguous().view({bsz, seqlen, -1});
+  scores = torch::softmax(scores.to(torch::kFloat), /*dim=*/-1).type_as(query);
+  return torch::einsum("bhqk,bkhd->bqhd", {scores, value})
+      .reshape({bsz, seqlen, -1});
 }
 
 }  // namespace llm
