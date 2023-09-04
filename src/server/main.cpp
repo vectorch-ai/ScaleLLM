afdf77f40221f3f2a32177362b619dc7cb30b7ab
Michael Mi <guocuimi@gmail.com>
2023-09-04 16:49:20 -0400

    fix weird link errors when building protobuf with libtorch.

---
+++ src/server/main.cpp
@@ -0,0 +1,33 @@
+#include <folly/init/Init.h>
+#include <gflags/gflags.h>
+#include <glog/logging.h>
+
+#include <thread>
+
+#include "grpc_server.h"
+
+using namespace llm;
+
+int main(int argc, char** argv) {
+  // glog and glfag will be initialized in folly::init
+  folly::init(&argc, &argv);
+
+  GrpcServer server;
+
+  GrpcServer::Options options;
+  options.address = "localhost";
+  options.port = 8888;
+
+  if (!server.start(options)) {
+    LOG(ERROR) << "failed to start grpc server";
+    return -1;
+  }
+
+  // TODO: add graceful shutdown
+  while (true) {
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    // TODO: update server status
+  }
+  server.stop();
+  return 0;
+}
