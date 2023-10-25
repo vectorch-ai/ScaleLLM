e08a71e7fd4ea81bc06395ebf0c6865307b0b530
Michael Mi <guocuimi@gmail.com>
2023-10-24 21:19:35 -0400

    fix CJKTest for SP tokenizer

--- src/tokenizer/sentencepiece_tokenizer_test.cpp
+++ src/tokenizer/sentencepiece_tokenizer_test.cpp
@@ -4,29 +4,27 @@
 
 namespace llm {
 
-TEST(SentencePieceTokenizerTest, EncodeTest) {
-  SentencePieceTokenizer tokenizer("data/test_model.model");
+TEST(SentencePieceTokenizerTest, EncodeDecodeTest) {
+  SentencePieceTokenizer tokenizer("data/tokenizer.model");
   std::vector<int> ids;
   ASSERT_TRUE(tokenizer.encode("Hello, world!", &ids));
-  const std::vector<int> desired_ids = {1, 151, 88, 21, 5, 887, 147};
+  const std::vector<int> desired_ids = {1, 15043, 29892, 3186, 29991};
   EXPECT_EQ(ids, desired_ids);
-}
 
-TEST(SentencePieceTokenizerTest, DecodeTest) {
-  SentencePieceTokenizer tokenizer("data/test_model.model");
-  const std::vector<int> tokens = {1, 151, 88, 21, 5, 887, 147};
-  const auto text = tokenizer.decode(tokens);
+  const auto text = tokenizer.decode(ids);
   EXPECT_EQ(text, "Hello, world!");
 }
 
 TEST(SentencePieceTokenizerTest, CJKTest) {
-  SentencePieceTokenizer tokenizer("data/test_model.model");
-  // const std::wstring test_text = L"你好，世界！";
-  // const auto ids = tokenizer.Encode(test_text);
-  // const std::vector<int> desired_ids = {4, 0, 5, 0, 147};
-  // EXPECT_EQ(ids, desired_ids);
+  SentencePieceTokenizer tokenizer("data/tokenizer.model");
+  const std::string test_text = "你好，世界！";
+  std::vector<int> ids;
+  ASSERT_TRUE(tokenizer.encode(test_text, &ids));
+  const std::vector<int> desired_ids = {
+      1, 29871, 30919, 31076, 30214, 30793, 30967, 30584};
+  EXPECT_EQ(ids, desired_ids);
 
-  // const auto text = tokenizer.Decode(ids);
-  // EXPECT_EQ(text, test_text);
+  const auto decoded_text = tokenizer.decode(ids);
+  EXPECT_EQ(decoded_text, test_text);
 }
 }  // namespace llm
