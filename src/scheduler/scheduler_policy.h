18e27eb42d9827a5336e8c47557f7e7f294867d8
Tongxuan Liu <tongxuan.ltx@mail.ustc.edu.cn>
2023-12-27 05:43:51 -0500

    [feat] implement speculative_scheduler and refine scheduler_policy.

--- src/scheduler/scheduler_policy.h
+++ src/scheduler/scheduler_policy.h
@@ -1,35 +1,32 @@
 #pragma once
 
-#include <absl/time/time.h>
 #include <folly/MPMCQueue.h>
 
 #include <cstdint>
 #include <memory>
-#include <queue>
 
 namespace llm {
 
 class Request;
+class Sequence; 
 class SchedulerPolicy {
  public:
   virtual ~SchedulerPolicy() = default;
 
-  virtual bool try_emplace(std::unique_ptr<Request>& request) = 0;
-  virtual void schedule() = 0;
+  virtual bool schedule(std::unique_ptr<Request>& request) = 0;
+  virtual std::vector<Sequence*> build_batch() = 0;
 };
 
 class BlockManager;
 class ResponseHandler;
-class Sequence; 
-// First come first serve scheduler policy
 class FCFSSchedulerPolicy final : public SchedulerPolicy {
  public:
   explicit FCFSSchedulerPolicy(ResponseHandler* response_handler,
                                BlockManager* block_manager);
   ~FCFSSchedulerPolicy() override;
 
-  bool try_emplace(std::unique_ptr<Request>& request) override;
-  void schedule() override;
+  bool schedule(std::unique_ptr<Request>& request) override;
+  std::vector<Sequence*> build_batch() override;
 
  private:
   ResponseHandler* response_handler_;
