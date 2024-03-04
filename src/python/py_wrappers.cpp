7b54808ba4d31cc706a343e9bac019d112d3afba
Tongxuan Liu <tongxuan.ltx@mail.ustc.edu.cn>
2024-03-04 13:06:08 +0000

    add py_wrapper to support python interface.

---
+++ src/python/py_wrappers.cpp
@@ -0,0 +1,20 @@
+#include <pybind11/pybind11.h>
+
+#include "server/llm.h"
+
+int add(int i, int j) {
+  return i + j;
+}
+
+PYBIND11_MODULE(gen_py_wrappers, m) {
+  // Add class LLM
+  pybind11::class_<llm::LLM, std::shared_ptr<llm::LLM>>(m, "LLM")
+    .def(pybind11::init<const std::string&, const llm::SamplingParameter&,
+                  const llm::StoppingCriteria&, int64_t,
+                  const std::string>())
+    .def(pybind11::init<const std::string>())
+    .def("generate", &llm::LLM::generate);
+
+  // Add function add
+  m.def("add", &add, "A function which adds two numbers");
+}
