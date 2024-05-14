4cbd05e9c3e62ae85d5a3ef21175c4d5b034ba31
Michael Mi <guocuimi@gmail.com>
2024-05-14 13:41:21 -0700

    [fix] added cuda 11.8 support for manylinux

--- docker/common/install_cmake.sh
+++ docker/common/install_cmake.sh
@@ -19,13 +19,12 @@ case "$ID" in
     ;;
 esac
 
-# Turn 3.6.3 into v3.6
-path=$(echo "${CMAKE_VERSION}" | sed -e 's/\([0-9].[0-9]\+\).*/v\1/')
-file="cmake-${CMAKE_VERSION}-Linux-x86_64.tar.gz"
+path="v${CMAKE_VERSION}"
+file="cmake-${CMAKE_VERSION}-linux-x86_64.tar.gz"
 
 # Download and install specific CMake version in /usr/local
 pushd /tmp
-curl -Os --retry 3 "https://cmake.org/files/${path}/${file}"
-tar -C /usr/local --strip-components 1 --no-same-owner -zxf cmake-*.tar.gz
+wget -q "https://github.com/Kitware/CMake/releases/download/${path}/${file}"
+tar -C /usr/local --strip-components 1 --no-same-owner -zxf ${file}
 rm -f cmake-*.tar.gz
 popd
