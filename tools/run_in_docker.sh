610a88220ada08c24b85aac9ac88ac3f92a01e25
Michael Mi <guocuimi@gmail.com>
2024-06-28 21:35:42 -0700

    dev: fix issues in run_in_docker script

--- tools/run_in_docker.sh
+++ tools/run_in_docker.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
-#  adapted from https://github.com/tensorflow/serving
+# Adapted from https://github.com/tensorflow/serving
 
 # ==============================================================================
 #
 # Script to run commands (like builds and scripts) in a docker container.
 #
-#
 # Note: This script binds your working directory (via pwd) and /tmp to the
 # Docker container. Any scripts or programs you run will need to have its
 # output files/dirs written to one of the above locations for persistence.
 #
-# Typical usage (to build from lastest upstream source):
+# Typical usage (to build from latest upstream source):
 # $ git clone --recursive https://github.com/vectorch-ai/ScaleLLM.git
 # $ cd ScaleLLM
 # $ ./tools/run_in_docker.sh cmake -G Ninja -S . -B build
@@ -51,29 +50,29 @@ RUN_OPTS+=("-v $(pwd):$(pwd)")
 RUN_OPTS+=("-v /tmp:/tmp")
 RUN_OPTS+=("-v ${HOME}:${HOME}")
 
-# run as the current user
+# Carry over cache settings
+# Carry over cache settings
+VCPKG_DEFAULT_BINARY_CACHE=${VCPKG_DEFAULT_BINARY_CACHE:-${HOME}/.cache/vcpkg}
+mkdir -p "${VCPKG_DEFAULT_BINARY_CACHE}"
+RUN_OPTS+=("-v ${VCPKG_DEFAULT_BINARY_CACHE}:${VCPKG_DEFAULT_BINARY_CACHE}")
+RUN_OPTS+=("-e VCPKG_DEFAULT_BINARY_CACHE=${VCPKG_DEFAULT_BINARY_CACHE}")
+
+CCACHE_DIR=${CCACHE_DIR:-${HOME}/.cache/ccache}
+mkdir -p "${CCACHE_DIR}"
+RUN_OPTS+=("-v ${CCACHE_DIR}:${CCACHE_DIR}")
+RUN_OPTS+=("-e CCACHE_DIR=${CCACHE_DIR}")
+
+# Run as the current user
 RUN_OPTS+=("-u $(id -u):$(id -g)")
 
-# carry over cache settings
-if [[ -n "${VCPKG_DEFAULT_BINARY_CACHE}" ]]; then
-  mkdir -p ${VCPKG_DEFAULT_BINARY_CACHE}
-  RUN_OPTS+=("-v ${VCPKG_DEFAULT_BINARY_CACHE}:${VCPKG_DEFAULT_BINARY_CACHE}")
-  RUN_OPTS+=("-e VCPKG_DEFAULT_BINARY_CACHE=${VCPKG_DEFAULT_BINARY_CACHE}")
-fi
-
-if [[ -n "${CCACHE_DIR}" ]]; then
-  mkdir -p ${CCACHE_DIR}
-  RUN_OPTS+=("-v ${CCACHE_DIR}:${CCACHE_DIR}")
-  RUN_OPTS+=("-e CCACHE_DIR=${CCACHE_DIR}")
-fi
-
 CMD="$@"
-[[ "${CMD}" = "" ]] && usage
+[[ "${CMD}" == "" ]] && usage
 
 [[ ! -x $(command -v docker) ]] && echo "ERROR: 'docker' command missing from PATH." && usage
 if ! docker pull ${IMAGE}; then
   echo "WARNING: Failed to docker pull image ${IMAGE}"
 fi
 
+# Execute the command in the Docker container
 # echo "docker run ${RUN_OPTS[@]} ${IMAGE} bash -c \"cd $(pwd); ${CMD}\""
 docker run ${RUN_OPTS[@]} ${IMAGE} bash -c "cd $(pwd); ${CMD}"
