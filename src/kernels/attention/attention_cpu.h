e9e63b0d158e6500306b3116843c528c92657dfc
Michael Mi <guocuimi@gmail.com>
2024-07-09 10:36:09 -0700

    kernel: added attention cpu implementation for testing (#268)

---
+++ src/kernels/attention/attention_cpu.h
@@ -0,0 +1,104 @@
+#pragma once
+
+#include <glog/logging.h>
+#include <torch/torch.h>
+
+#include <algorithm>
+#include <cstdint>
+#include <cute/tensor.hpp>
+
+#include "common/range.h"
+#include "cute/layout.hpp"
+#include "cute/stride.hpp"
+
+namespace llm {
+// query/out: [q_seq_len, n_head, head_dim]
+// key:   [seq_len, n_kv_head, head_dim]
+// value: [seq_len, n_kv_head, head_dim]
+inline void mha(torch::Tensor query,
+                torch::Tensor key,
+                torch::Tensor value,
+                torch::Tensor& out) {
+  CHECK_EQ(query.dim(), 3);
+  CHECK_EQ(key.dim(), 3);
+  CHECK_EQ(key.sizes(), value.sizes()) << "k and v must have the same shape";
+  CHECK_EQ(query.size(2), key.size(2)) << "q and k must have the same head_dim";
+
+  const int64_t q_seq_len = query.size(0);
+  const int64_t n_heads = query.size(1);
+  const int64_t head_dim = query.size(2);
+  const int64_t seq_len = key.size(0);
+  const int64_t n_kv_heads = key.size(1);
+  CHECK_LE(q_seq_len, seq_len) << "q_len must be less than or equal to seq_len";
+  CHECK_LE(n_kv_heads, n_heads)
+      << "n_kv_heads must be less than or equal to n_heads";
+  CHECK(n_heads % n_kv_heads == 0) << "n_heads must be divisible by n_kv_heads";
+  // number of heads to share the same k/v head
+  const int64_t group_size = n_heads / n_kv_heads;
+  const int64_t q_seq_base = seq_len - q_seq_len;
+  const float sm_scale = static_cast<float>(1.0 / std::sqrt(head_dim));
+
+  // convert to cute tensors
+  using namespace cute;
+  // [q_seq_len, n_heads, head_dim]
+  auto qo_shape = make_shape(q_seq_len, n_heads, head_dim);
+  auto g_q = make_tensor(query.data_ptr<float>(), qo_shape, GenRowMajor{});
+  auto g_o = make_tensor(out.data_ptr<float>(), qo_shape, GenRowMajor{});
+  // [seq_len, n_kv_heads, head_dim]
+  auto kv_shape = make_shape(seq_len, n_kv_heads, head_dim);
+  auto g_k = make_tensor(key.data_ptr<float>(), kv_shape, GenRowMajor{});
+  auto g_v = make_tensor(value.data_ptr<float>(), kv_shape, GenRowMajor{});
+
+  // allocate intermediate storage
+  std::vector<float> storage(seq_len);
+
+  // process each query against all key/value in sequence
+  for (int64_t q_idx : range(q_seq_len)) {
+    // process each head independently
+    for (int64_t q_head_idx : range(n_heads)) {
+      // corresponding key-value head index
+      const int64_t kv_head_idx = q_head_idx / group_size;
+
+      // slice query, key, value, and output for current head
+      auto q = g_q(q_idx, q_head_idx, _);             // [head_dim]
+      auto o = g_o(q_idx, q_head_idx, _);             // [head_dim]
+      auto k = g_k(_, kv_head_idx, _);                // [seq_len, head_dim]
+      auto v = g_v(_, kv_head_idx, _);                // [seq_len, head_dim]
+      auto s = make_tensor(storage.data(), seq_len);  // [seq_len]
+
+      // dot product q and k: s = q * k
+      float max = -INFINITY;
+      cute::fill(s, 0.0f);
+      for (int64_t kv_idx : range(seq_len)) {
+        for (int64_t d : range(head_dim)) {
+          s(kv_idx) += q(d) * k(kv_idx, d) * sm_scale;
+        }
+        // apply causal mask
+        if (kv_idx > q_seq_base + q_idx) {
+          s(kv_idx) = -INFINITY;
+        }
+        max = std::max(max, s(kv_idx));
+      }
+
+      // safe softmax: attn = exp(attn - max_score) / sum(...)
+      float sum = 0;
+      for (int64_t kv_idx : range(seq_len)) {
+        s(kv_idx) = std::exp(s(kv_idx) - max);
+        sum += s(kv_idx);
+      }
+      for (int64_t kv_idx : range(seq_len)) {
+        s(kv_idx) /= sum;
+      }
+
+      // dot product s and v: o = s * v
+      cute::fill(o, 0.0f);
+      for (int64_t kv_idx : range(seq_len)) {
+        for (int64_t d : range(head_dim)) {
+          o[d] += s(kv_idx) * v(kv_idx, d);
+        }
+      }
+    }
+  }
+}
+
+}  // namespace llm
---
+++ src/kernels/attention/attention_test.cpp
@@ -0,0 +1,83 @@
+#include <ATen/ops/equal.h>
+#include <gtest/gtest.h>
+#include <torch/csrc/autograd/generated/variable_factories.h>
+
+#include "attention_cpu.h"
+
+namespace llm {
+namespace {
+// Multi-head attention implementation using pytorch
+torch::Tensor masked_self_attention(
+    torch::Tensor query,  // [q_seq_len, n_heads, head_dim]
+    torch::Tensor key,    // [seq_len, n_kv_heads, head_dim]
+    torch::Tensor value   // [seq_len, n_kv_heads, head_dim]
+) {
+  const auto q_seq_len = query.size(0);
+  const auto n_heads = query.size(1);
+  const auto head_dim = query.size(2);
+  const auto seq_len = key.size(0);
+  const auto n_kv_heads = key.size(1);
+
+  // repeat key and value if n_kv_heads < n_heads
+  if (n_kv_heads < n_heads) {
+    CHECK_EQ(n_heads % n_kv_heads, 0);
+    const auto n_groups = n_heads / n_kv_heads;
+    key = key.repeat_interleave(/*repeats=*/n_groups, /*dim=*/1);
+    value = value.repeat_interleave(/*repeats=*/n_groups, /*dim=*/1);
+  }
+
+  // query * key => [n_heads, q_seq_len, seq_len]
+  auto scores = torch::einsum("qhd,khd->hqk", {query, key});
+  // apply scale
+  const float sm_scale = static_cast<float>(1.0 / std::sqrt(head_dim));
+  scores *= sm_scale;
+
+  // apply causal mask
+  torch::Tensor mask = torch::ones({1, q_seq_len, seq_len}, torch::kBool);
+  // returns the lower triangular part of a matrix
+  mask = torch::tril(mask, /*diagonal=*/seq_len - q_seq_len).to(query);
+  scores = scores.masked_fill(mask == 0, -INFINITY);
+
+  // safe softmax
+  scores = torch::softmax(scores, /*dim=*/-1);
+
+  // score * value => [q_seq_len, n_heads, head_dim]
+  return torch::einsum("hqk,khd->qhd", {scores, value});
+}
+
+}  // namespace
+
+class AttentionCPUTest
+    : public ::testing::TestWithParam<std::tuple<int64_t /*seq_len*/,
+                                                 int64_t /*q_seq_len*/,
+                                                 int64_t /*n_heads*/,
+                                                 int64_t /*n_kv_heads*/,
+                                                 int64_t /*head_dim*/>> {};
+
+TEST_P(AttentionCPUTest, MHA) {
+  const auto [seq_len, q_seq_len, n_heads, n_kv_heads, head_dim] = GetParam();
+
+  const auto options = torch::dtype(torch::kFloat).device(torch::kCPU);
+
+  const auto query = torch::randn({q_seq_len, n_heads, head_dim}, options);
+  const auto key = torch::randn({seq_len, n_kv_heads, head_dim}, options);
+  const auto value = torch::randn({seq_len, n_kv_heads, head_dim}, options);
+
+  auto ref_out = masked_self_attention(query, key, value);
+
+  auto out = torch::empty_like(query);
+  mha(query, key, value, out);
+  EXPECT_TRUE(torch::allclose(out, ref_out, /*rtol=*/1e-5, /*atol=*/1e-5));
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    MHA,
+    AttentionCPUTest,
+    ::testing::Combine(::testing::Values(20),       // seq_len
+                       ::testing::Values(10, 20),   // q_seq_len
+                       ::testing::Values(6),        // n_heads
+                       ::testing::Values(6, 3, 1),  // n_kv_heads
+                       ::testing::Values(32)        // head_dim
+                       ));
+
+}  // namespace llm
