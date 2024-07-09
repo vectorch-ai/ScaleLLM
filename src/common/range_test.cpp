3ffdc24798b5f78274ee5bb305687393c24ef729
Michael Mi <guocuimi@gmail.com>
2024-07-08 23:48:37 -0700

    feat: added range to support Range-for loops (#267)

---
+++ src/common/range_test.cpp
@@ -0,0 +1,32 @@
+#include "range.h"
+
+#include <gtest/gtest.h>
+
+namespace llm {
+TEST(RangeTest, EmptyRange) {
+  for (int i : range<int>(0)) {
+    GTEST_FAIL() << "Empty range should not have any elements";
+  }
+
+  for (int i : range<int>(4, 2)) {
+    GTEST_FAIL() << "Empty range should not have any elements";
+  }
+}
+
+TEST(RangeTest, Basic) {
+  std::vector<int> expected = {0, 1, 2, 3, 4};
+  std::vector<int> actual;
+  for (int i : range<int>(5)) {
+    actual.push_back(i);
+  }
+  EXPECT_EQ(expected, actual);
+
+  expected = {2, 3};
+  actual.clear();
+  for (int i : range<int>(2, 4)) {
+    actual.push_back(i);
+  }
+  EXPECT_EQ(expected, actual);
+}
+
+}  // namespace llm
