4938c830dd7c0f1ab4ad0f1ff1b28cd8e8a1fc63
Michael Mi <guocuimi@gmail.com>
2024-08-02 16:04:32 -0700

    model: added qwen2 support (#295)

---
+++ src/models/huggingface/qwen2.h
@@ -0,0 +1,443 @@
+#pragma once
+
+#include <torch/torch.h>
+
+#include <string>
+#include <vector>
+
+#include "chat_template/coded_chat_template.h"
+#include "layers/activation.h"
+#include "layers/attention/attention.h"
+#include "layers/attention/handler.h"
+#include "layers/embedding.h"
+#include "layers/linear.h"
+#include "layers/normalization.h"
+#include "layers/qkv_linear.h"
+#include "memory/kv_cache.h"
+#include "models/model_args.h"
+#include "models/model_registry.h"
+#include "models/parameters.h"
+
+// QWen2 model compatible with huggingface weights
+// ref to:
+// https://github.com/huggingface/transformers/blob/v4.43.3/src/transformers/models/qwen2/modeling_qwen2.py
+namespace llm::hf {
+
+class QWen2MLPImpl : public torch::nn::Module {
+ public:
+  QWen2MLPImpl(const ModelArgs& args,
+               const QuantArgs& quant_args,
+               const ParallelArgs& parallel_args,
+               const torch::TensorOptions& options) {
+    act_with_mul_ = Activation::get_act_with_mul_func("silu", options.device());
+    CHECK(act_with_mul_ != nullptr);
+
+    const int64_t hidden_size = args.hidden_size();
+    const int64_t intermediate_size = args.intermediate_size();
+
+    // register the weight parameter
+    gate_up_proj_ =
+        register_module("gate_up_proj",
+                        ColumnParallelLinear(hidden_size,
+                                             intermediate_size * 2,
+                                             /*bias=*/false,
+                                             /*gather_output=*/false,
+                                             quant_args,
+                                             parallel_args,
+                                             options));
+    down_proj_ =
+        register_module("down_proj",
+                        RowParallelLinear(intermediate_size,
+                                          hidden_size,
+                                          /*bias=*/false,
+                                          /*input_is_parallelized=*/true,
+                                          quant_args,
+                                          parallel_args,
+                                          options));
+  }
+
+  torch::Tensor forward(torch::Tensor x) {
+    return down_proj_(act_with_mul_(gate_up_proj_(x)));
+  }
+
+  // load the weight from the checkpoint
+  void load_state_dict(const StateDict& state_dict) {
+    // call each submodule's load_state_dict function
+    gate_up_proj_->load_state_dict(state_dict, {"gate_proj.", "up_proj."});
+    down_proj_->load_state_dict(state_dict.select("down_proj."));
+  }
+
+  void verify_loaded_weights(const std::string& prefix) const {
+    gate_up_proj_->verify_loaded_weights(prefix + "[gate_proj,up_proj].");
+    down_proj_->verify_loaded_weights(prefix + "down_proj.");
+  }
+
+ private:
+  // parameter members, must be registered
+  ColumnParallelLinear gate_up_proj_{nullptr};
+  RowParallelLinear down_proj_{nullptr};
+
+  // calculate act(x) * y
+  ActFunc act_with_mul_{nullptr};
+};
+TORCH_MODULE(QWen2MLP);
+
+class QWen2AttentionImpl : public torch::nn::Module {
+ public:
+  QWen2AttentionImpl(const ModelArgs& args,
+                     const QuantArgs& quant_args,
+                     const ParallelArgs& parallel_args,
+                     const torch::TensorOptions& options,
+                     AttentionHandler* handler) {
+    const int32_t world_size = parallel_args.world_size();
+    const int64_t hidden_size = args.hidden_size();
+    const int64_t n_heads = args.n_heads();
+    const int64_t n_kv_heads = args.n_kv_heads().value_or(n_heads);
+    const int64_t head_dim = args.head_dim();
+    const int64_t n_local_heads = n_heads / world_size;
+    const int64_t n_local_kv_heads =
+        std::max<int64_t>(1, n_kv_heads / world_size);
+
+    // size for q, k, v
+    qkv_sizes_ = {n_local_heads * head_dim,
+                  n_local_kv_heads * head_dim,
+                  n_local_kv_heads * head_dim};
+
+    // register submodules
+    qkv_proj_ = register_module("qkv_proj",
+                                QKVColumnParallelLinear(hidden_size,
+                                                        n_heads,
+                                                        n_kv_heads,
+                                                        head_dim,
+                                                        /*bias=*/true,
+                                                        /*gather_output=*/false,
+                                                        quant_args,
+                                                        parallel_args,
+                                                        options));
+
+    o_proj_ = register_module("o_proj",
+                              RowParallelLinear(hidden_size,
+                                                hidden_size,
+                                                /*bias=*/false,
+                                                /*input_is_parallelized=*/true,
+                                                quant_args,
+                                                parallel_args,
+                                                options));
+
+    // initialize attention
+    atten_ = register_module(
+        "atten", Attention(n_local_heads, n_local_kv_heads, head_dim, handler));
+  }
+
+  torch::Tensor forward(torch::Tensor x,
+                        torch::Tensor positions,
+                        KVCache& kv_cache,
+                        const InputParameters& input_params) {
+    // (num_tokens, dim) x (dim, n_local_heads * head_dim)
+    // => (num_tokens, n_local_heads * head_dim)
+    auto qkv = qkv_proj_(x).split(/*split_size=*/qkv_sizes_, /*dim=*/-1);
+    DCHECK_EQ(qkv.size(), 3);
+
+    // calculate attention, output: (num_tokens, n_local_heads * head_dim)
+    auto output =
+        atten_(qkv[0], qkv[1], qkv[2], positions, kv_cache, input_params);
+    return o_proj_(output);
+  }
+
+  // load the weight from the checkpoint
+  void load_state_dict(const StateDict& state_dict) {
+    // call each submodule's load_state_dict function
+    qkv_proj_->load_state_dict(
+        state_dict, {"q_proj.", "k_proj.", "v_proj."}, {"k_proj.", "v_proj."});
+    o_proj_->load_state_dict(state_dict.select("o_proj."));
+  }
+
+  void verify_loaded_weights(const std::string& prefix) const {
+    qkv_proj_->verify_loaded_weights(prefix + "[q_proj,k_proj,v_proj].");
+    o_proj_->verify_loaded_weights(prefix + "o_proj.");
+  }
+
+ private:
+  // parameter members, must be registered
+  QKVColumnParallelLinear qkv_proj_{nullptr};
+
+  RowParallelLinear o_proj_{nullptr};
+
+  // module members without parameters
+  Attention atten_{nullptr};
+
+  // size for q, k, v
+  std::vector<int64_t> qkv_sizes_;
+};
+TORCH_MODULE(QWen2Attention);
+
+class QWen2DecoderLayerImpl : public torch::nn::Module {
+ public:
+  QWen2DecoderLayerImpl(const ModelArgs& args,
+                        const QuantArgs& quant_args,
+                        const ParallelArgs& parallel_args,
+                        const torch::TensorOptions& options,
+                        AttentionHandler* handler) {
+    // register submodules
+    self_attn_ = register_module(
+        "self_attn",
+        QWen2Attention(args, quant_args, parallel_args, options, handler));
+    mlp_ = register_module("mlp",
+                           QWen2MLP(args, quant_args, parallel_args, options));
+
+    input_layernorm_ = register_module(
+        "input_layernorm",
+        RMSNormResidual(args.hidden_size(), args.rms_norm_eps(), options));
+    post_attention_layernorm_ = register_module(
+        "post_attention_layernorm",
+        RMSNormResidual(args.hidden_size(), args.rms_norm_eps(), options));
+  }
+
+  torch::Tensor forward(torch::Tensor x,
+                        torch::Tensor positions,
+                        torch::Tensor& residual,
+                        KVCache& kv_cache,
+                        const InputParameters& input_params) {
+    auto hidden_states = input_layernorm_(x, residual);
+
+    hidden_states =
+        self_attn_(hidden_states, positions, kv_cache, input_params);
+    hidden_states = post_attention_layernorm_(hidden_states, residual);
+    hidden_states = mlp_(hidden_states);
+    return hidden_states;
+  }
+
+  // load the weight from the checkpoint
+  void load_state_dict(const StateDict& state_dict) {
+    // call each submodule's load_state_dict function
+    self_attn_->load_state_dict(state_dict.select("self_attn."));
+    mlp_->load_state_dict(state_dict.select("mlp."));
+    input_layernorm_->load_state_dict(state_dict.select("input_layernorm."));
+    post_attention_layernorm_->load_state_dict(
+        state_dict.select("post_attention_layernorm."));
+  }
+
+  void verify_loaded_weights(const std::string& prefix) const {
+    self_attn_->verify_loaded_weights(prefix + "self_attn.");
+    mlp_->verify_loaded_weights(prefix + "mlp.");
+    input_layernorm_->verify_loaded_weights(prefix + "input_layernorm.");
+    post_attention_layernorm_->verify_loaded_weights(
+        prefix + "post_attention_layernorm.");
+  }
+
+ private:
+  // parameter members, must be registered
+  QWen2Attention self_attn_{nullptr};
+
+  QWen2MLP mlp_{nullptr};
+
+  // RSM Norm
+  RMSNormResidual input_layernorm_{nullptr};
+  RMSNormResidual post_attention_layernorm_{nullptr};
+};
+TORCH_MODULE(QWen2DecoderLayer);
+
+class QWen2ModelImpl : public torch::nn::Module {
+ public:
+  QWen2ModelImpl(const ModelArgs& args,
+                 const QuantArgs& quant_args,
+                 const ParallelArgs& parallel_args,
+                 const torch::TensorOptions& options) {
+    // register submodules
+    embed_tokens_ = register_module(
+        "embed_tokens",
+        ParallelEmbedding(
+            args.vocab_size(), args.hidden_size(), parallel_args, options));
+
+    handler_ = AttentionHandler::create_handler_with_rope(
+        args, /*interleaved=*/false, options);
+
+    blocks_ = register_module("layers", torch::nn::ModuleList());
+    layers_.reserve(args.n_layers());
+    for (int32_t i = 0; i < args.n_layers(); i++) {
+      auto block = QWen2DecoderLayer(
+          args, quant_args, parallel_args, options, handler_.get());
+      layers_.push_back(block);
+      blocks_->push_back(block);
+    }
+    norm_ = register_module(
+        "norm",
+        RMSNormResidual(args.hidden_size(), args.rms_norm_eps(), options));
+  }
+
+  // tokens: [num_tokens]
+  // positions: [num_tokens] token pos in the sequence
+  torch::Tensor forward(torch::Tensor tokens,
+                        torch::Tensor positions,
+                        std::vector<KVCache>& kv_caches,
+                        const InputParameters& input_params) {
+    auto h = embed_tokens_(tokens);
+    torch::Tensor residual;
+
+    // TODO: set working space for attention handler
+    for (size_t i = 0; i < layers_.size(); i++) {
+      auto& layer = layers_[i];
+      h = layer(h, positions, residual, kv_caches[i], input_params);
+    }
+    return norm_(h, residual);
+  }
+
+  // load the weight from the checkpoint
+  void load_state_dict(const StateDict& state_dict) {
+    embed_tokens_->load_state_dict(state_dict.select("embed_tokens."));
+    // call each layer's load_state_dict function
+    for (int i = 0; i < layers_.size(); i++) {
+      layers_[i]->load_state_dict(
+          state_dict.select("layers." + std::to_string(i) + "."));
+    }
+    norm_->load_state_dict(state_dict.select("norm."));
+  }
+
+  void verify_loaded_weights(const std::string& prefix) const {
+    embed_tokens_->verify_loaded_weights(prefix + "embed_tokens.");
+    for (int i = 0; i < layers_.size(); i++) {
+      layers_[i]->verify_loaded_weights(prefix + "layers." + std::to_string(i) +
+                                        ".");
+    }
+    norm_->verify_loaded_weights(prefix + "norm.");
+  }
+
+ private:
+  // parameter members, must be registered
+  ParallelEmbedding embed_tokens_{nullptr};
+
+  // attention handler
+  std::unique_ptr<AttentionHandler> handler_{nullptr};
+
+  torch::nn::ModuleList blocks_{nullptr};
+  // hold same data but different type as blocks_ to avoid type cast
+  std::vector<QWen2DecoderLayer> layers_;
+
+  RMSNormResidual norm_{nullptr};
+};
+TORCH_MODULE(QWen2Model);
+
+class QWen2ForCausalLMImpl : public torch::nn::Module {
+ public:
+  QWen2ForCausalLMImpl(const ModelArgs& args,
+                       const QuantArgs& quant_args,
+                       const ParallelArgs& parallel_args,
+                       const torch::TensorOptions& options) {
+    // register submodules
+    model_ = register_module(
+        "model", QWen2Model(args, quant_args, parallel_args, options));
+
+    lm_head_ = register_module("lm_head",
+                               ColumnParallelLinear(args.hidden_size(),
+                                                    args.vocab_size(),
+                                                    /*bias=*/false,
+                                                    /*gather_output=*/true,
+                                                    parallel_args,
+                                                    options));
+  }
+
+  // tokens: [num_tokens]
+  // positions: [num_tokens] token pos in the sequence
+  // returns: [num_tokens, hidden_size]
+  torch::Tensor forward(const torch::Tensor& tokens,
+                        const torch::Tensor& positions,
+                        std::vector<KVCache>& kv_caches,
+                        const InputParameters& input_params) {
+    return model_(tokens, positions, kv_caches, input_params);
+  }
+
+  // hidden_states: [num_tokens, hidden_size]
+  // seleted_idxes: [num_tokens]
+  // returns: [num_tokens, vocab_size]
+  torch::Tensor logits(const torch::Tensor& hidden_states,
+                       const torch::Tensor& seleted_idxes) {
+    // select tokens if provided
+    auto h = hidden_states;
+    if (seleted_idxes.defined()) {
+      h = h.index_select(/*dim=*/0, seleted_idxes);
+    }
+    return lm_head_(h);
+  }
+
+  // load the weight from the checkpoint
+  void load_state_dict(const StateDict& state_dict) {
+    model_->load_state_dict(state_dict.select("model."));
+    lm_head_->load_state_dict(state_dict.select("lm_head."));
+  }
+
+  void verify_loaded_weights() const {
+    model_->verify_loaded_weights("model.");
+    lm_head_->verify_loaded_weights("lm_head.");
+  }
+
+ private:
+  // parameter members, must be registered
+  QWen2Model model_{nullptr};
+
+  ColumnParallelLinear lm_head_{nullptr};
+};
+TORCH_MODULE(QWen2ForCausalLM);
+
+class QWen2ChatTemplate final : public CodedChatTemplate {
+ public:
+  // Prompt template:
+  // <|im_start|>user\n {message} <|im_end|>\n
+  // <|im_start|>assistant\n
+  std::optional<std::string> get_prompt(
+      const std::string_view& system_message,
+      const std::vector<std::string_view>& messages) const override {
+    // at least one user message
+    if (messages.size() % 2 == 0) {
+      return std::nullopt;
+    }
+
+    std::stringstream ss;
+    if (!system_message.empty()) {
+      ss << "<|im_start|>system\n" << system_message << "<|im_end|>\n";
+    }
+
+    // then user and assistant message pairs (u/a/u/a/u...)
+    for (size_t i = 0; i < messages.size(); ++i) {
+      const char* role = (i % 2) == 0 ? "user" : "assistant";
+      ss << "<|im_start|>" << role << "\n" << messages[i] << "<|im_end|>\n";
+    }
+    // end with assistant message
+    ss << "<|im_start|>assistant\n";
+    return ss.str();
+  }
+};
+
+// register the causal model
+REGISTER_CAUSAL_MODEL(qwen2, QWen2ForCausalLM);
+REGISTER_DEFAULT_CHAT_TEMPLATE(qwen2, QWen2ChatTemplate);
+// register the model args
+// example config:
+// https://huggingface.co/Qwen/Qwen2-7B-Instruct/blob/main/config.json
+REGISTER_MODEL_ARGS(qwen2, [&] {
+  LOAD_ARG_OR(model_type, "model_type", "QWen2");
+  LOAD_ARG_OR(dtype, "torch_dtype", "");
+  LOAD_ARG_OR(vocab_size, "vocab_size", 152064);
+  LOAD_ARG_OR(hidden_size, "hidden_size", 3584);
+  LOAD_ARG_OR(n_layers, "num_hidden_layers", 28);
+  LOAD_ARG_OR(n_heads, "num_attention_heads", 28);
+  LOAD_ARG(n_kv_heads, "num_key_value_heads");
+  // LOAD_ARG_OR(no_bias, "no_bias", true);
+  LOAD_ARG_OR(intermediate_size, "intermediate_size", 18944);
+  LOAD_ARG_OR(max_position_embeddings, "max_position_embeddings", 32768);
+  LOAD_ARG_OR(rms_norm_eps, "rms_norm_eps", 1e-6);
+  LOAD_ARG_OR(bos_token_id, "bos_token_id", 151643);
+  LOAD_ARG_OR(eos_token_id, "eos_token_id", 151645);
+  LOAD_ARG_OR(rope_theta, "rope_theta", 1000000.0f);
+
+  // TODO: sliding window support: use_sliding_window, sliding_window
+
+  LOAD_ARG_OR_FUNC(head_dim, "head_dim", [&] {
+    return args->hidden_size() / args->n_heads();
+  });
+
+  // stop token ids: "<|endoftext|>", "<|im_start|>", "<|im_end|>"
+  SET_ARG(stop_token_ids,
+          std::unordered_set<int32_t>({151643, 151644, 151645}));
+});
+
+}  // namespace llm::hf
