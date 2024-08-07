c97758e064dd5d51b60ec46e87171fe2e4d88bfd
Michael Mi <guocuimi@gmail.com>
2024-08-07 12:13:01 -0700

    kernel: support kernel test in python via pybind (#301)

--- scalellm/csrc/module.cpp
+++ scalellm/csrc/module.cpp
@@ -11,6 +11,7 @@ namespace py = pybind11;
 extern void init_sampling_params(py::module_& m);
 extern void init_output(py::module_& m);
 extern void init_llm_handler(py::module_& m);
+extern void init_kernels(py::module_& m);
 
 // NOLINTNEXTLINE
 static std::string get_metrics() { return Metrics::Instance().GetString(); }
@@ -26,6 +27,10 @@ PYBIND11_MODULE(PY_MODULE_NAME, m) {
   init_sampling_params(m);
   init_output(m);
   init_llm_handler(m);
+
+  py::module_ kernels =
+      m.def_submodule("kernels", "ScaleLLM kernel ops submodule");
+  init_kernels(kernels);
 }
 
 }  // namespace llm::csrc
