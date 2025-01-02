53758247bb6967d67bb4c04ed7d0151d7851a1c9
Michael Mi <guocuimi@gmail.com>
2025-01-01 21:48:49 -0800

    kernel: refactor attention kernel for readibility

--- src/kernels/attention/attention_kernel_sm80.cuh
+++ src/kernels/attention/attention_kernel_sm80.cuh
@@ -92,24 +92,6 @@ __global__ void mha_kernel_sm80(void* o,
   // (BLK_K, BLK_N), k-major
   Tensor sVt = make_tensor(make_smem_ptr(v_smem), SmemLayoutVt{});
 
-  // Fragments for GEMM
-  TiledMMA tiled_mma;
-  auto thr_mma = tiled_mma.get_slice(tidx);
-  // GEMM-I: S = Q@K.T
-  auto tSrQ = thr_mma.partition_fragment_A(sQ);  // (MMA,MMA_M,MMA_K)
-  auto tSrK = thr_mma.partition_fragment_B(sK);  // (MMA,MMA_N,MMA_K)
-  auto tSrAccS = partition_fragment_C(
-      tiled_mma, Shape<BLK_M, BLK_N>{});  // (MMA,MMA_M,MMA_N)
-
-  // GEMM-II: O = softmax(S)@V
-  auto tOrVt = thr_mma.partition_fragment_B(sVt);  // (MMA,MMA_K,MMA_N)
-  auto tOrAccO = partition_fragment_C(
-      tiled_mma, Shape<BLK_M, HEAD_DIM>{});  // (MMA,MMA_M,MMA_K)
-
-  // reshape for iterating over rows and columns
-  auto tOrAccO_rc_view = Convertor::to_rowcol(tOrAccO);
-  auto tSrAccS_rc_view = Convertor::to_rowcol(tSrAccS);
-
   // Tiled Copy
   // g2s tiled copy for qkv
   GmemTiledCopyQKV gmem_tiled_copy_QKV;
@@ -120,6 +102,33 @@ __global__ void mha_kernel_sm80(void* o,
   auto tKsK = gmem_thr_copy_QKV.partition_D(sK);
   auto tVgV = gmem_thr_copy_QKV.partition_S(gV(_, _, 0));
   auto tVsV = gmem_thr_copy_QKV.partition_D(sV);
+
+  auto produce_q = [&]() {
+    cute::copy(gmem_tiled_copy_QKV, tQgQ, tQsQ);
+    cp_async_fence();
+  };
+
+  auto produce_k = [&](int ni) {
+    gK = local_tile(K, make_tile(BLK_N{}, HEAD_DIM{}), make_coord(ni, _));
+    tKgK = gmem_thr_copy_QKV.partition_S(gK(_, _, 0));
+    cute::copy(gmem_tiled_copy_QKV, tKgK, tKsK);
+    cp_async_fence();
+  };
+
+  auto produce_v = [&](int ni) {
+    gV = local_tile(V, make_tile(BLK_N{}, HEAD_DIM{}), make_coord(ni, _));
+    tVgV = gmem_thr_copy_QKV.partition_S(gV(_, _, 0));
+    cute::copy(gmem_tiled_copy_QKV, tVgV, tVsV);
+    cp_async_fence();
+  };
+
+  TiledMMA tiled_mma;
+  auto thr_mma = tiled_mma.get_slice(tidx);
+  // GEMM-I: S = Q@K.T
+  auto tSrQ = thr_mma.partition_fragment_A(sQ);  // (MMA,MMA_M,MMA_K)
+  auto tSrK = thr_mma.partition_fragment_B(sK);  // (MMA,MMA_N,MMA_K)
+  auto tSrAccS = partition_fragment_C(
+      tiled_mma, Shape<BLK_M, BLK_N>{});  // (MMA,MMA_M,MMA_N)
 
   // s2r tiled copy for qkv
   SmemTiledCopyQ smem_tiled_copy_Q;
@@ -132,84 +141,28 @@ __global__ void mha_kernel_sm80(void* o,
   auto tSsK = smem_thr_copy_K.partition_S(sK);
   auto tSrK_copy_view = smem_thr_copy_K.retile_D(tSrK);
 
-  SmemTiledCopyVT smem_tiled_copy_Vt;
-  auto smem_thr_copy_Vt = smem_tiled_copy_Vt.get_thread_slice(tidx);
-  auto tOsVt = smem_thr_copy_Vt.partition_S(sVt);
-  auto tOrVt_copy_view = smem_thr_copy_Vt.retile_D(tOrVt);
-
-  // ###############  Prologue  ###############
-
-  // produce q: [] => [q]
-  cute::copy(gmem_tiled_copy_QKV, tQgQ, tQsQ);
-  cp_async_fence();
-
-  // produce k: [q] => [q, k]
-  cute::copy(gmem_tiled_copy_QKV, tKgK, tKsK);
-  cp_async_fence();
-
-  // wait q: [q, k] => [k]
-  cp_async_wait<1>();
-  __syncthreads();
-
-  // apply sm_scale
-  // TODO: use thread parallelism
-  for (int i = 0; i < size(tQsQ); ++i) {
-    tQsQ(i) = Element(tQsQ(i) * sm_scale);
-  }
-
-  // RowsPerThread = #rows_per_MMA * #MMA_M
-  constexpr int RowsPerThread = 2 * size<1>(tOrAccO);
-  OnlineSoftmax<RowsPerThread> softmax;
-
-  // ###############  Mainloop  ###############
-
-  const int n_block_min = 0;
-  const int n_block_max = cute::ceil_div(kv_len, BLK_N{});
-
-  // clear output
-  clear(tOrAccO);
-  CUTE_NO_UNROLL
-  for (int ni = n_block_min; ni < n_block_max; ++ni) {
-    // clear attention score for each block
-    clear(tSrAccS);
-
-    // wait k, queue: [q, k] => []
-    cp_async_wait<0>();
-    __syncthreads();
-
-    // produce v, [] => [v]
-    {
-      gV = local_tile(V, make_tile(BLK_N{}, HEAD_DIM{}), make_coord(ni, _));
-      tVgV = gmem_thr_copy_QKV.partition_S(gV(_, _, 0));
-      cute::copy(gmem_tiled_copy_QKV, tVgV, tVsV);
-    }
-    cp_async_fence();
-
-    // 1> S = Q@K.T
+  // S = Q@K.T
+  auto compute_qk = [&]() {
     CUTE_UNROLL
     for (int ki = 0; ki < size<2>(tSrQ); ++ki) {
       cute::copy(smem_tiled_copy_Q, tSsQ(_, _, ki), tSrQ_copy_view(_, _, ki));
       cute::copy(smem_tiled_copy_K, tSsK(_, _, ki), tSrK_copy_view(_, _, ki));
       cute::gemm(tiled_mma, tSrQ(_, _, ki), tSrK(_, _, ki), tSrAccS);
     }
+  };
 
-    // apply softmax and rescale
-    softmax.rescale(tSrAccS_rc_view, tOrAccO_rc_view);
+  // GEMM-II: O = softmax(S)@V
+  auto tOrVt = thr_mma.partition_fragment_B(sVt);  // (MMA,MMA_K,MMA_N)
+  auto tOrAccO = partition_fragment_C(
+      tiled_mma, Shape<BLK_M, HEAD_DIM>{});  // (MMA,MMA_M,MMA_K)
 
-    // wait v, [v] => []
-    cp_async_wait<0>();
-    __syncthreads();
+  SmemTiledCopyVT smem_tiled_copy_Vt;
+  auto smem_thr_copy_Vt = smem_tiled_copy_Vt.get_thread_slice(tidx);
+  auto tOsVt = smem_thr_copy_Vt.partition_S(sVt);
+  auto tOrVt_copy_view = smem_thr_copy_Vt.retile_D(tOrVt);
 
-    // produce next k: [] => [k]
-    if (ni != n_block_max - 1) {
-      gK = local_tile(K, make_tile(BLK_N{}, HEAD_DIM{}), make_coord(ni + 1, _));
-      tKgK = gmem_thr_copy_QKV.partition_S(gK(_, _, 0));
-      cute::copy(gmem_tiled_copy_QKV, tKgK, tKsK);
-    }
-    cp_async_fence();
-
-    // 2> O = softmax(S)*V
-
+  // O = softmax(S)*V
+  auto compute_sv = [&]() {
     // cast scores from Accumulator to Element
     auto tSrS = make_tensor_like<Element>(tSrAccS);
     CUTE_UNROLL
@@ -226,6 +179,92 @@ __global__ void mha_kernel_sm80(void* o,
           smem_tiled_copy_Vt, tOsVt(_, _, ki), tOrVt_copy_view(_, _, ki));
       cute::gemm(tiled_mma, tOrS(_, _, ki), tOrVt(_, _, ki), tOrAccO);
     }
+  };
+
+  // write output to gmem
+  auto epilogue = [&]() {
+    // 1> covernt output from ElementAccumulator to Element
+    auto tOrO = make_tensor_like<Element>(tOrAccO);
+    CUTE_UNROLL
+    for (int si = 0; si < size(tOrAccO); ++si) {
+      tOrO(si) = static_cast<Element>(tOrAccO(si));
+    }
+
+    // 2. copy output from reg to smem (reuse sQ)
+    auto sO = make_tensor(sQ.data(), SmemLayoutO{});
+
+    SmemTiledCopyO smem_tiled_copy_O;
+    auto smem_thr_copy_O = smem_tiled_copy_O.get_thread_slice(tidx);
+    // ((Atom,AtomNum),MMA_M,MMA_N)
+    auto taccOrO = smem_thr_copy_O.retile_S(tOrO);
+    // ((Atom,AtomNum),PIPE_M,PIPE_N)
+    auto taccOsO = smem_thr_copy_O.partition_D(sO);
+    cute::copy(smem_tiled_copy_O, taccOrO, taccOsO);
+
+    // 3. copy output from smem to gmem
+    GmemTiledCopyO gmem_tiled_copy_O;
+    auto gmem_thr_copy_O = gmem_tiled_copy_O.get_thread_slice(tidx);
+    // ((Atom,AtomNum),ATOM_M,ATOM_N)
+    auto tOsO = gmem_thr_copy_O.partition_S(sO);
+    auto tOgO = gmem_thr_copy_O.partition_D(gO(_, _, 0));
+
+    // wait for smem copy before copy to gmem
+    __syncthreads();
+    cute::copy(gmem_tiled_copy_O, tOsO, tOgO);
+  };
+
+  // ###############  Prologue  ###############
+
+  // produce q: [] => [q]
+  produce_q();
+  // produce k: [q] => [q, k]
+  produce_k(0);
+
+  // ###############  Mainloop  ###############
+
+  // reshape for iterating over rows and columns
+  auto tOrAccO_rc_view = Convertor::to_rowcol(tOrAccO);
+  auto tSrAccS_rc_view = Convertor::to_rowcol(tSrAccS);
+
+  // clear output
+  clear(tOrAccO);
+
+  // RowsPerThread = #rows_per_MMA * #MMA_M
+  constexpr int RowsPerThread = 2 * size<1>(tOrAccO);
+  OnlineSoftmax<RowsPerThread> softmax(sm_scale);
+
+  const int n_block_min = 0;
+  const int n_block_max = cute::ceil_div(kv_len, BLK_N{});
+
+  CUTE_NO_UNROLL
+  for (int ni = n_block_min; ni < n_block_max; ++ni) {
+    // clear attention score for each block
+    clear(tSrAccS);
+
+    // wait k, queue: [q, k] => []
+    cp_async_wait<0>();
+    __syncthreads();
+
+    // produce v, [] => [v]
+    produce_v(ni);
+
+    // 1> S = Q@K.T
+    compute_qk();
+
+    // apply softmax and rescale
+    softmax.rescale(tSrAccS_rc_view, tOrAccO_rc_view);
+
+    // wait v, [v] => []
+    cp_async_wait<0>();
+    __syncthreads();
+
+    // produce next k: [] => [k]
+    if (ni != n_block_max - 1) {
+      produce_k(ni + 1);
+    }
+
+    // 2> O = softmax(S)*V
+    compute_sv();
   }
 
   // ###############  Epilogue  ###############
@@ -234,34 +273,7 @@ __global__ void mha_kernel_sm80(void* o,
   softmax.finalize(tOrAccO_rc_view);
 
   // write output to gmem
-  // 1> covernt output from ElementAccumulator to Element
-  auto tOrO = make_tensor_like<Element>(tOrAccO);
-  CUTE_UNROLL
-  for (int si = 0; si < size(tOrAccO); ++si) {
-    tOrO(si) = static_cast<Element>(tOrAccO(si));
-  }
-
-  // 2. copy output from reg to smem
-  auto sO = make_tensor(sQ.data(), SmemLayoutO{});
-
-  SmemTiledCopyO smem_tiled_copy_O;
-  auto smem_thr_copy_O = smem_tiled_copy_O.get_thread_slice(tidx);
-  // ((Atom,AtomNum),MMA_M,MMA_N)
-  auto taccOrO = smem_thr_copy_O.retile_S(tOrO);
-  // ((Atom,AtomNum),PIPE_M,PIPE_N)
-  auto taccOsO = smem_thr_copy_O.partition_D(sO);
-  cute::copy(smem_tiled_copy_O, taccOrO, taccOsO);
-
-  // 3. copy output from smem to gmem
-  GmemTiledCopyO gmem_tiled_copy_O;
-  auto gmem_thr_copy_O = gmem_tiled_copy_O.get_thread_slice(tidx);
-  // ((Atom,AtomNum),ATOM_M,ATOM_N)
-  auto tOsO = gmem_thr_copy_O.partition_S(sO);
-  auto tOgO = gmem_thr_copy_O.partition_D(gO(_, _, 0));
-
-  // wait for smem copy before copy to gmem
-  __syncthreads();
-  cute::copy(gmem_tiled_copy_O, tOsO, tOgO);
+  epilogue();
 }
 
 }  // namespace llm
