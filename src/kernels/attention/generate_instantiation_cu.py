62dcc3e8be77964a0bc3e76daa1d7415d84f63f8
Michael Mi <guocuimi@gmail.com>
2025-01-16 11:31:42 -0800

    update

--- src/kernels/attention/generate_instantiation_cu.py
+++ src/kernels/attention/generate_instantiation_cu.py
@@ -1,20 +1,19 @@
 #!/usr/bin/env python3
 
 # This file is run to generate the kernel instantiations for the attention kernels
-# They are written to several files in order to speed up compilation
 import shutil
 import itertools
 from dataclasses import dataclass
 from pathlib import Path
-from typing import Generator, List
+from typing import Iterator
 
 DTYPE_MAP = {
     "fp16": "cute::half_t",
     "bf16": "cute::bfloat16_t",
 }
 
-SM = [80]  # Sm80 kernels support up to
 HEAD_DIMENSIONS = [64, 128, 256]
+
 PAGEDKV_KERNEL_IMPL_TEMPLATE = """
 #include "attention_launch_sm80.cuh" // IWYU pragma: keep
 
@@ -27,10 +26,8 @@ template void run_attention_kernel_sm80<{DTYPE}, {HEAD_DIM}, Params>(
 }}  // namespace llm
 """
 
-
 @dataclass
 class Kernel:
-    sm: int
     dtype: str
     head_dim: int
 
@@ -42,21 +39,19 @@ class Kernel:
 
     @property
     def filename(self) -> str:
-        return f"attention_{self.dtype}_hdim{self.head_dim}_sm{self.sm}.cu"
+        return f"attention_{self.dtype}_hd{self.head_dim}_sm80.cu"
 
 
-def get_all_kernels() -> List[Kernel]:
-    for dtype, head_dim, sm in itertools.product(DTYPE_MAP.keys(), HEAD_DIMENSIONS, SM):
-        yield Kernel(sm=sm, dtype=dtype, head_dim=head_dim)
-
-
-def write_kernel(kernel: Kernel, autogen_dir: Path) -> None:
-    (autogen_dir / kernel.filename).write_text(kernel.template)
+def get_all_kernels() -> Iterator[Kernel]:
+    for dtype, head_dim in itertools.product(DTYPE_MAP.keys(), HEAD_DIMENSIONS):
+        yield Kernel(dtype=dtype, head_dim=head_dim)
 
 
 if __name__ == "__main__":
     output_dir = Path.cwd() / "generated"
     shutil.rmtree(output_dir, ignore_errors=True)
     output_dir.mkdir(parents=True, exist_ok=True)
+    
+    # written to several files to speed up compilation
     for kernel in get_all_kernels():
-        write_kernel(kernel, output_dir)
+        (output_dir / kernel.filename).write_text(kernel.template)
