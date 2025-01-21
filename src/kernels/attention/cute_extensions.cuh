d48995b64331de222410e17c04d082a0874a440d
Michael Mi <guocuimi@gmail.com>
2025-01-20 17:27:26 -0800

    upgrade cutlass to 3.7.0

--- src/kernels/attention/cute_extensions.cuh
+++ src/kernels/attention/cute_extensions.cuh
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <cute/atom/mma_atom.hpp>
-#include <cute/swizzle_layout.hpp>
 #include <cute/tensor.hpp>
 
 #include "cute/config.hpp"
@@ -9,24 +8,6 @@
 #include "cute/layout_composed.hpp"
 
 namespace cute {
-
-template <class... Args, class ATensor>
-CUTE_HOST_DEVICE constexpr auto partition_fragment_A(
-    const ThrMMA<Args...>& thr_mma,
-    const ATensor& atensor) {
-  auto atensor_noswizzle =
-      make_tensor(atensor.data(), get_nonswizzle_portion(atensor.layout()));
-  return thr_mma.partition_fragment_A(atensor_noswizzle);
-}
-
-template <class... Args, class BTensor>
-CUTE_HOST_DEVICE constexpr auto partition_fragment_B(
-    const ThrMMA<Args...>& thr_mma,
-    const BTensor& btensor) {
-  auto btensor_noswizzle =
-      make_tensor(btensor.data(), get_nonswizzle_portion(btensor.layout()));
-  return thr_mma.partition_fragment_B(btensor_noswizzle);
-}
 
 template <size_t I, class IntTupleA, class IntTupleB>
 CUTE_HOST_DEVICE constexpr auto elem_less(IntTupleA const& a,
--- src/kernels/attention/tools/attention_traits_viewer.cpp
+++ src/kernels/attention/tools/attention_traits_viewer.cpp
@@ -7,6 +7,12 @@
 using namespace cute;
 using namespace llm;
 
+template <class... Args>
+void save_svg(const std::string& filename, Args&&... args) {
+  std::ofstream os(filename);
+  print_svg(os, args...);
+}
+
 template <typename Traits>
 void print_attn_traits() {
   // type alias
@@ -19,7 +25,8 @@ void print_attn_traits() {
   using SmemLayoutVt = typename Traits::SmemLayoutVt;
   using SmemLayoutO = typename Traits::SmemLayoutO;
 
-  using GmemTiledCopyQKV = typename Traits::GmemTiledCopyQKV;
+  using GmemTiledCopyQ = typename Traits::GmemTiledCopyQ;
+  using GmemTiledCopyKV = typename Traits::GmemTiledCopyKV;
   using GmemTiledCopyO = typename Traits::GmemTiledCopyO;
 
   using SmemTiledCopyQ = typename Traits::SmemTiledCopyQ;
@@ -31,97 +38,80 @@ void print_attn_traits() {
   print("TiledMma: \n");
   print(TiledMma{});
   print("\n\n");
-  {
-    std::ofstream os("tiled_mma.svg");
-    print_svg(os, TiledMma{});
-  }
+  save_svg("tiled_mma.svg", TiledMma{});
 
   // print g2s tiled copy Q
-  print("GmemTiledCopyQKV: \n");
-  print(GmemTiledCopyQKV{});
+  print("GmemTiledCopyQ: \n");
+  print(GmemTiledCopyQ{});
   print("\n\n");
-  {
-    std::ofstream os("g2s_tiled_copy_q.svg");
-    print_svg(os, GmemTiledCopyQKV{});
-  }
+  save_svg("g2s_tiled_copy_q.svg", GmemTiledCopyQ{});
+
+  // print g2s tiled copy KV
+  print("GmemTiledCopyKV: \n");
+  print(GmemTiledCopyKV{});
+  print("\n\n");
+  save_svg("g2s_tiled_copy_kv.svg", GmemTiledCopyKV{});
+
   // print s2g tiled copy O
   print("GmemTiledCopyO: \n");
   print(GmemTiledCopyO{});
   print("\n\n");
-  {
-    std::ofstream os("s2g_tiled_copy_o.svg");
-    print_svg(os, GmemTiledCopyO{});
-  }
+  save_svg("s2g_tiled_copy_o.svg", GmemTiledCopyO{});
 
   // print s2r tiled copy Q
   print("SmemTiledCopyQ: \n");
   print(SmemTiledCopyQ{});
   print("\n\n");
-  {
-    std::ofstream os("s2r_tiled_copy_q.svg");
-    print_svg(os, SmemTiledCopyQ{});
-  }
+  save_svg("s2r_tiled_copy_q.svg", SmemTiledCopyQ{});
+
   // print s2r tiled copy K
   print("SmemTiledCopyK: \n");
   print(SmemTiledCopyK{});
   print("\n\n");
-  {
-    std::ofstream os("s2r_tiled_copy_k.svg");
-    print_svg(os, SmemTiledCopyK{});
-  }
+  save_svg("s2r_tiled_copy_k.svg", SmemTiledCopyK{});
+
   // print s2r tiled copy Vt
   print("SmemTiledCopyVt: \n");
   print(SmemTiledCopyVt{});
   print("\n\n");
-  {
-    std::ofstream os("s2r_tiled_copy_vt.svg");
-    print_svg(os, SmemTiledCopyVt{});
-  }
+  save_svg("s2r_tiled_copy_vt.svg", SmemTiledCopyVt{});
+
   // print r2s tiled copy O
   print("SmemTiledCopyO: \n");
   print(SmemTiledCopyO{});
   print("\n\n");
-  {
-    std::ofstream os("r2s_tiled_copy_o.svg");
-    print_svg(os, SmemTiledCopyO{});
-  }
+  save_svg("r2s_tiled_copy_o.svg", SmemTiledCopyO{});
 
   // print smem layout Q
   print("SmemLayoutQ: \n");
   print(SmemLayoutQ{});
   print("\n\n");
-  {
-    std::ofstream os("smem_layout_q.svg");
-    print_svg(os, SmemLayoutQ{}, GmemTiledCopyQKV{}, SmemTiledCopyQ{});
-  }
+  save_svg(
+      "smem_layout_q.svg", SmemLayoutQ{}, GmemTiledCopyQ{}, SmemTiledCopyQ{});
+
   // print smem layout KV
   print("SmemLayoutK: \n");
   print(SmemLayoutK{});
   print("\n\n");
-  {
-    std::ofstream os("smem_layout_k.svg");
-    print_svg(os, SmemLayoutK{}, GmemTiledCopyQKV{}, SmemTiledCopyK{});
-  }
+  save_svg(
+      "smem_layout_k.svg", SmemLayoutK{}, GmemTiledCopyKV{}, SmemTiledCopyK{});
+
   // print smem layout Vt
   print("SmemLayoutVt: \n");
   print(SmemLayoutVt{});
   print("\n\n");
-  {
-    std::ofstream os("smem_layout_vt.svg");
-    print_svg(os,
-              SmemLayoutV{},
-              SmemLayoutVt{},
-              GmemTiledCopyQKV{},
-              SmemTiledCopyVt{});
-  }
+  save_svg("smem_layout_vt.svg",
+           SmemLayoutV{},
+           SmemLayoutVt{},
+           GmemTiledCopyKV{},
+           SmemTiledCopyVt{});
+
   // print smem layout O
   print("SmemLayoutO: \n");
   print(SmemLayoutO{});
   print("\n\n");
-  {
-    std::ofstream os("smem_layout_o.svg");
-    print_svg(os, SmemLayoutO{}, SmemTiledCopyO{}, GmemTiledCopyO{});
-  }
+  save_svg(
+      "smem_layout_o.svg", SmemLayoutO{}, SmemTiledCopyO{}, GmemTiledCopyO{});
 }
 
 int main(int argc, char** argv) {
