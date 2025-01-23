1dd4dca89f5d586c07597e586d2ccf80cf135203
Michael Mi <guocuimi@gmail.com>
2025-01-22 21:04:49 -0800

    merge traits

--- src/kernels/attention/attention_traits_sm80.h
+++ src/kernels/attention/attention_traits_sm80.h
@@ -29,6 +29,77 @@ struct LayoutConvertor {
   }
 };
 
+template <typename DType, typename KV_DType>
+CUTE_HOST_DEVICE constexpr auto tiled_mma_selector() {
+  CUTE_STATIC_ASSERT(
+      sizeof_bits<KV_DType>::value == 16 || sizeof_bits<KV_DType>::value == 8,
+      "KV_DType must be 8 or 16 bits");
+
+  using MMA_Atom = std::conditional_t<std::is_same_v<DType, cute::half_t>,
+                                      MMA_Atom<SM80_16x8x16_F32F16F16F32_TN>,
+                                      MMA_Atom<SM80_16x8x16_F32BF16BF16F32_TN>>;
+  using TiledMma = TiledMMA<MMA_Atom,
+                            Layout<Shape<_4, _1, _1>>,  // warp layout 4x1x1
+                            Tile<_64, _16, _16>>;       // Prom Shape 64x16x16
+  return TiledMma{};
+}
+
+template <typename COPY_Atom, int BLK_K, int THREADS>
+CUTE_HOST_DEVICE constexpr auto tiled_copy_selector() {
+  using DType = typename COPY_Atom::ValType;
+  // use 128 bits vectorized copy
+  constexpr int kValPerThr = 128 / sizeof_bits_v<DType>;
+  constexpr int kThrsPerRow = BLK_K / kValPerThr;
+  using ThrLayout = Layout<Shape<Int<THREADS / kThrsPerRow>, Int<kThrsPerRow>>,
+                           Stride<Int<kThrsPerRow>, _1>>;
+  using ValLayout = Layout<Shape<_1, Int<kValPerThr>>>;
+  return make_tiled_copy(COPY_Atom{}, ThrLayout{}, ValLayout{});
+}
+
+template <typename KV_DType, typename TiledMma>
+CUTE_HOST_DEVICE constexpr auto tiled_copy_B_selector() {
+  if constexpr (sizeof_bits_v<KV_DType> == 16) {
+    return make_tiled_copy_B(Copy_Atom<SM75_U32x4_LDSM_N, KV_DType>{},
+                             TiledMma{});
+  } else if constexpr (sizeof_bits_v<KV_DType> == 8) {
+    // ((_4, _8), (_4, _2)):((_64, _1), (_16, _8))
+    using Layout_TV_K = Layout<Shape<Shape<_4, _8>, Shape<_4, _2>>,
+                               Stride<Stride<_64, _1>, Stride<_16, _8>>>;
+    // use cute::uint8_t as InternalType
+    using SmemTiledCopyK =
+        TiledCopy<Copy_Atom<SM75_U32x2_LDSM_N, cute::uint8_t>,
+                  Layout_TV_K,
+                  Shape<_16, _16>>;  // N x K
+    return SmemTiledCopyK{};
+  } else {
+    CUTE_STATIC_ASSERT_V(
+        sizeof_bits_v<KV_DType> == 8 || sizeof_bits_v<KV_DType> == 16,
+        "KV_DType must be 8 or 16 bits");
+  }
+}
+
+template <typename DType, typename TiledMma>
+CUTE_HOST_DEVICE constexpr auto tiled_copy_B_T_selector() {
+  if constexpr (sizeof_bits_v<DType> == 16) {
+    return make_tiled_copy_B(Copy_Atom<SM75_U16x8_LDSM_T, DType>{}, TiledMma{});
+  } else if constexpr (sizeof_bits_v<DType> == 8) {
+    // ((_4, _8), (_4, _2)):((_64, _1), (_16, _8))
+    // ((_4, _8), (_2, _2, _2)):((_32, _2), (_1, _16, _128))
+    using Layout_TV_Vt = Layout<Shape<Shape<_4, _8>, Shape<_2, _2, _2>>,
+                                Stride<Stride<_32, _2>, Stride<_1, _16, _128>>>;
+    // use cute::uint8_t as InternalType
+    using SmemTiledCopyVt =
+        TiledCopy<Copy_Atom<SM75_U16x4_LDSM_T, cute::uint8_t>,
+                  Layout_TV_Vt,
+                  Shape<_16, _16>>;  // K x N
+    return SmemTiledCopyVt{};
+  } else {
+    CUTE_STATIC_ASSERT_V(
+        sizeof_bits_v<DType> == 8 || sizeof_bits_v<DType> == 16,
+        "DType must be 8 or 16 bits");
+  }
+}
+
 }  // namespace detail
 
 template <typename DTYPE,
@@ -55,13 +126,8 @@ struct AttentionTraitsSM80 {
   // ******* Mainloop *******
   // TiledMMA (64x16x16) for gemm-I and gemm-II
   // choose MMA_Atom based on Element type
-  using MMA_Atom_ =
-      std::conditional_t<std::is_same_v<DType, cute::half_t>,
-                         MMA_Atom<SM80_16x8x16_F32F16F16F32_TN>,
-                         MMA_Atom<SM80_16x8x16_F32BF16BF16F32_TN>>;
-  using TiledMma = TiledMMA<MMA_Atom_,
-                            Layout<Shape<_4, _1, _1>>,  // warp layout 4x1x1
-                            Tile<_64, _16, _16>>;       // Prom Shape 64x16x16
+  using TiledMma = decltype(detail::tiled_mma_selector<DType, KV_DType>());
+  static constexpr size_t kThreadNum = size(TiledMma{});
 
   // Layout convertor for TiledMMA (64x16x16)
   using LayoutConvertor = detail::LayoutConvertor;
@@ -83,45 +149,37 @@ struct AttentionTraitsSM80 {
   using SmemLayoutV =
       decltype(tile_to_shape(SmemLayoutAtom{}, Shape<_BLK_N, _HEAD_DIM>{}));
 
-  // V^T smem: (HEAD_DIM, BLK_N) row-major
+  // V^T smem (transpose view of V): (HEAD_DIM, BLK_N)
   using SmemLayoutVt = decltype(composition(
       SmemLayoutV{},
       make_layout(Shape<_HEAD_DIM, _BLK_N>{}, GenRowMajor{})));
 
-  // Thr layout for gmem copy
-  using GmemCopyThrLayout =
-      std::conditional_t<BLK_K == 32,
-                         Layout<Shape<_32, _4>, Stride<_4, _1>>,
-                         Layout<Shape<_16, _8>, Stride<_8, _1>>>;
-
   // Tiled copy for QKV
   // g2s tiled copy for q
-  using GmemTiledCopyQ = decltype(make_tiled_copy(
-      Copy_Atom<SM80_CP_ASYNC_CACHEGLOBAL<cute::uint128_t>, DType>{},
-      GmemCopyThrLayout{},     // Thr layout: (_16,_8)/(_32, _4)
-      Layout<Shape<_1, _8>>{}  // Val layout: 8 vals per read
-      ));
+  using GmemTiledCopyQ =
+      decltype(detail::tiled_copy_selector<
+               Copy_Atom<SM80_CP_ASYNC_CACHEGLOBAL<cute::uint128_t>, DType>,
+               BLK_K,
+               kThreadNum>());
 
   // g2s tiled copy for kv
   // TODO: choose based on BLK_K and kv cache type
-  using GmemTiledCopyKV = decltype(make_tiled_copy(
-      Copy_Atom<SM80_CP_ASYNC_CACHEGLOBAL<cute::uint128_t>, KV_DType>{},
-      GmemCopyThrLayout{},     // Thr layout: (_16,_8)/(_32, _4)
-      Layout<Shape<_1, _8>>{}  // Val layout: 8 vals per read
-      ));
+  using GmemTiledCopyKV =
+      decltype(detail::tiled_copy_selector<
+               Copy_Atom<SM80_CP_ASYNC_CACHEGLOBAL<cute::uint128_t>, KV_DType>,
+               BLK_K,
+               kThreadNum>());
 
   // s2r tiled copy for gemm-I
   using SmemTiledCopyQ =
       decltype(make_tiled_copy_A(Copy_Atom<SM75_U32x4_LDSM_N, DType>{},
                                  TiledMma{}));
   using SmemTiledCopyK =
-      decltype(make_tiled_copy_B(Copy_Atom<SM75_U32x4_LDSM_N, KV_DType>{},
-                                 TiledMma{}));
+      decltype(detail::tiled_copy_B_selector<KV_DType, TiledMma>());
 
   // s2r tiled copy for gemm-II
   using SmemTiledCopyVt =
-      decltype(make_tiled_copy_B(Copy_Atom<SM75_U16x8_LDSM_T, KV_DType>{},
-                                 TiledMma{}));
+      decltype(detail::tiled_copy_B_T_selector<KV_DType, TiledMma>());
 
   // ******* Epilogue *******
 
@@ -129,11 +187,10 @@ struct AttentionTraitsSM80 {
   using SmemLayoutO = SmemLayoutQ;
 
   // s2g tiled copy for O
-  using GmemTiledCopyO = decltype(make_tiled_copy(
-      Copy_Atom<DefaultCopy, DType>{},
-      GmemCopyThrLayout{},     // Thr layout: (_16,_8)/(_32, _4)
-      Layout<Shape<_1, _8>>{}  // Val layout: 8 vals per read
-      ));
+  using GmemTiledCopyO =
+      decltype(detail::tiled_copy_selector<Copy_Atom<DefaultCopy, DType>,
+                                           BLK_K,
+                                           kThreadNum>());
 
   // r2s tiled copy for O
   using SmemTiledCopyO =
@@ -143,8 +200,6 @@ struct AttentionTraitsSM80 {
   static constexpr size_t kSmemSize =
       cosize(SmemLayoutQ{}) * sizeof(DType) +
       (cosize(SmemLayoutK{}) + cosize(SmemLayoutV{})) * sizeof(KV_DType);
-
-  static constexpr size_t kThreadNum = size(TiledMma{});
 };
 
 }  // namespace llm
