319641b0354eb42809ad46b9240b389718394d93
Michael Mi <guocuimi@gmail.com>
2025-04-08 20:14:27 -0700

    nccl: added all2all for nccl process group (#423)

--- src/model_parallel/process_group.cpp
+++ src/model_parallel/process_group.cpp
@@ -1,73 +1,96 @@
 #include "process_group.h"
 
-#include <ATen/cuda/CUDAEvent.h>
-#include <c10/core/Device.h>
 #include <c10/cuda/CUDAStream.h>
-#include <cuda_runtime.h>
 #include <glog/logging.h>
 #include <torch/torch.h>
 
+#include <cstdint>
 #include <memory>
 #include <vector>
 
 namespace llm {
 namespace {
 
-// NOLINTNEXTLINE(cppcoreguidelines-macro-usage)
-#define NCCLCHECK(cmd)                                               \
-  do {                                                               \
-    ncclResult_t r = cmd;                                            \
-    if (r != ncclSuccess) {                                          \
-      LOG(FATAL) << "Failed, NCCL error :" << ncclGetErrorString(r); \
-    }                                                                \
-  } while (0)
+inline void NCCL_CHECK(ncclResult_t result) {
+  if (result != ncclSuccess) {
+    LOG(FATAL) << "NCCL error: " << ncclGetErrorString(result);
+  }
+}
 
-// NOLINTNEXTLINE(cppcoreguidelines-macro-usage)
-#define CUDACHECK(cmd)                                                 \
-  do {                                                                 \
-    cudaError_t err = cmd;                                             \
-    if (err != cudaSuccess) {                                          \
-      LOG(FATAL) << "Failed, Cuda error :" << cudaGetErrorString(err); \
-    }                                                                  \
-  } while (0)
-
-at::Tensor flatten_for_scatter_gather(std::vector<at::Tensor>& tensors) {
+torch::Tensor flatten_for_scatter_gather(std::vector<torch::Tensor>& tensors) {
   auto& t = tensors[0];
   std::vector<int64_t> sizes{static_cast<int64_t>(tensors.size())};
   sizes.insert(sizes.end(), t.sizes().begin(), t.sizes().end());
-  return at::empty(sizes, t.options());
+  return torch::empty(sizes, t.options());
 }
 
 ncclDataType_t to_nccl_data_type(const torch::Tensor& input) {
   const auto type = input.scalar_type();
   switch (type) {
-    case at::kFloat:
+    case torch::kFloat:
       return ncclDataType_t::ncclFloat;
-    case at::kHalf:
+    case torch::kHalf:
       return ncclDataType_t::ncclHalf;
-    case at::kDouble:
+    case torch::kDouble:
       return ncclDataType_t::ncclDouble;
-    case at::kLong:
+    case torch::kLong:
       return ncclDataType_t::ncclInt64;
-    case at::kInt:
+    case torch::kInt:
       return ncclDataType_t::ncclInt;
-    case at::kChar:
+    case torch::kChar:
       return ncclDataType_t::ncclChar;
-    case at::kByte:
+    // NOLINTNEXTLINE(bugprone-branch-clone)
+    case torch::kByte:
       return ncclDataType_t::ncclUint8;
-    case at::kBool:
+    case torch::kBool:
       return ncclDataType_t::ncclUint8;
-    case at::kBFloat16:
+    case torch::kBFloat16:
       return ncclDataType_t::ncclBfloat16;
+    // NOLINTNEXTLINE(bugprone-branch-clone)
+    case torch::kFloat8_e4m3fn:
+      return ncclDataType_t::ncclUint8;
+    case torch::kFloat8_e5m2:
+      return ncclDataType_t::ncclUint8;
     default:
       TORCH_CHECK(false, "Unconvertible NCCL type ", type);
   }
 }
 
-void check_input(torch::Tensor input) {
+void check_input(const torch::Tensor& input) {
   CHECK(input.is_cuda()) << "input should be cuda tensor";
   CHECK(input.is_contiguous()) << "input should be contiguous";
   CHECK(!input.is_sparse()) << "input have to be cuda dense tensor";
+}
+
+void check_split_sizes(const std::vector<int64_t>& split_sizes,
+                       const torch::Tensor& tensor,
+                       int group_size) {
+  CHECK_EQ(split_sizes.size(), group_size)
+      << "split_sizes should have the same size as group_size";
+  int64_t size = 0;
+  for (int i = 0; i < group_size; ++i) {
+    size += split_sizes[i];
+  }
+  CHECK_EQ(size, tensor.size(0))
+      << "split sizes doesn't match total dim 0 size";
+}
+
+// Compute alltoall lengths and offsets
+void compute_lengths_and_offsets(const std::vector<int64_t>& split_sizes,
+                                 const torch::Tensor& tensor,
+                                 std::vector<size_t>* lengths,
+                                 std::vector<size_t>* offsets) {
+  size_t group_size = lengths->size();
+  size_t dim0_size = tensor.size(0);
+  size_t row_size = (dim0_size ? tensor.numel() / dim0_size : 1);
+
+  size_t offset = 0;
+  for (int i = 0; i < group_size; ++i) {
+    size_t length = row_size * split_sizes[i];
+    (*lengths)[i] = length;
+    (*offsets)[i] = offset;
+    offset += length;
+  }
 }
 
 }  // namespace
@@ -88,7 +111,7 @@ std::vector<std::unique_ptr<ProcessGroup>> ProcessGroup::create_process_groups(
 
   std::vector<ncclComm_t> comms(devices.size());
   const int world_size = static_cast<int>(devices.size());
-  NCCLCHECK(ncclCommInitAll(comms.data(), world_size, device_idxs.data()));
+  NCCL_CHECK(ncclCommInitAll(comms.data(), world_size, device_idxs.data()));
 
   std::vector<std::unique_ptr<ProcessGroup>> process_groups;
   process_groups.reserve(devices.size());
@@ -107,7 +130,7 @@ ProcessGroupNCCL::ProcessGroupNCCL(int rank,
     : ProcessGroup(rank, world_size, device), comm_(comm) {}
 
 // Destructor.
-ProcessGroupNCCL::~ProcessGroupNCCL() { NCCLCHECK(ncclCommDestroy(comm_)); }
+ProcessGroupNCCL::~ProcessGroupNCCL() { NCCL_CHECK(ncclCommDestroy(comm_)); }
 
 void ProcessGroupNCCL::allreduce(torch::Tensor& input) {
   DCHECK(input.device() == device())
@@ -118,9 +141,8 @@ void ProcessGroupNCCL::allreduce(torch::Tensor& input) {
   const auto count = input.numel();
   const auto data_type = to_nccl_data_type(input);
 
-  auto stream = at::cuda::getCurrentCUDAStream();
-  torch::DeviceGuard device_guard(device());
-  NCCLCHECK(ncclAllReduce(
+  cudaStream_t stream = at::cuda::getCurrentCUDAStream().stream();
+  NCCL_CHECK(ncclAllReduce(
       /*sendbuff=*/input.data_ptr(),
       /*recvbuff=*/input.data_ptr(),
       /*count=*/count,
@@ -130,7 +152,7 @@ void ProcessGroupNCCL::allreduce(torch::Tensor& input) {
       /*stream=*/stream));
 }
 
-void ProcessGroupNCCL::allgather(torch::Tensor input,
+void ProcessGroupNCCL::allgather(const torch::Tensor& input,
                                  std::vector<torch::Tensor>& outputs) {
   check_input(input);
   CHECK(outputs.size() == world_size())
@@ -138,14 +160,13 @@ void ProcessGroupNCCL::allgather(torch::Tensor input,
   DCHECK(input.device() == device())
       << "input should be on the same device as the process group";
 
-  torch::DeviceGuard device_guard(device());
   torch::Tensor flattened_output = flatten_for_scatter_gather(outputs);
 
   const auto count = input.numel();
   const auto data_type = to_nccl_data_type(input);
 
-  auto stream = at::cuda::getCurrentCUDAStream();
-  NCCLCHECK(ncclAllGather(
+  cudaStream_t stream = at::cuda::getCurrentCUDAStream().stream();
+  NCCL_CHECK(ncclAllGather(
       /*sendbuff=*/input.data_ptr(),
       /*recvbuff=*/flattened_output.data_ptr(),
       /*sendcount=*/count,
@@ -157,6 +178,117 @@ void ProcessGroupNCCL::allgather(torch::Tensor input,
   for (int i = 0; i < outputs.size(); ++i) {
     outputs[i].copy_(flattened_output[i], /*non_blocking=*/true);
   }
+}
+
+void ProcessGroupNCCL::allgather(const torch::Tensor& input,
+                                 torch::Tensor& output) {
+  check_input(input);
+  check_input(output);
+  CHECK(input.dtype() == output.dtype())
+      << "input and output should have the same dtype";
+  CHECK(input.numel() * world_size() == output.numel())
+      << "output should have the size of world_size times input tensor size";
+
+  const auto count = input.numel();
+  const auto data_type = to_nccl_data_type(input);
+
+  cudaStream_t stream = at::cuda::getCurrentCUDAStream().stream();
+  NCCL_CHECK(ncclAllGather(
+      /*sendbuff=*/input.data_ptr(),
+      /*recvbuff=*/output.data_ptr(),
+      /*sendcount=*/count,
+      /*datatype=*/data_type,
+      /*comm=*/comm_,
+      /*stream=*/stream));
+}
+void ProcessGroupNCCL::alltoall(const torch::Tensor& input,
+                                torch::Tensor& output) {
+  check_input(input);
+  check_input(output);
+  CHECK(input.dtype() == output.dtype())
+      << "input and output should have the same dtype";
+  DCHECK(input.device() == device())
+      << "input should be on the same device as the process group";
+
+  const int size = world_size();
+  CHECK_EQ(input.numel() % size, 0)
+      << "input should be divisible by world size";
+
+  ncclDataType_t type = to_nccl_data_type(input);
+  const size_t count = input.numel() / size;
+  const size_t rankdiff = input.nbytes() / size;
+
+  const char* sendbuff = reinterpret_cast<const char*>(input.const_data_ptr());
+  char* recvbuff = reinterpret_cast<char*>(output.data_ptr());
+  cudaStream_t stream = at::cuda::getCurrentCUDAStream().stream();
+
+  NCCL_CHECK(ncclGroupStart());
+  for (int r = 0; r < size; ++r) {
+    NCCL_CHECK(ncclSend(/*sendbuff=*/sendbuff + (r * rankdiff),
+                        /*count=*/count,
+                        /*datatype=*/type,
+                        /*peer=*/r,
+                        /*comm=*/comm_,
+                        /*stream=*/stream));
+    NCCL_CHECK(ncclRecv(/*recvbuff=*/recvbuff + (r * rankdiff),
+                        /*count=*/count,
+                        /*datatype=*/type,
+                        /*peer=*/r,
+                        /*comm=*/comm_,
+                        /*stream=*/stream));
+  }
+  NCCL_CHECK(ncclGroupEnd());
+}
+
+void ProcessGroupNCCL::alltoall(
+    const torch::Tensor& input,
+    torch::Tensor& output,
+    const std::vector<int64_t>& input_split_sizes,
+    const std::vector<int64_t>& output_split_sizes) {
+  check_input(input);
+  check_input(output);
+  CHECK(input.dtype() == output.dtype())
+      << "input and output should have the same dtype";
+  DCHECK(input.device() == device())
+      << "input should be on the same device as the process group";
+
+  const int size = world_size();
+  check_split_sizes(input_split_sizes, input, size);
+  check_split_sizes(output_split_sizes, output, size);
+
+  const auto ele_size = input.element_size();
+  const auto type = to_nccl_data_type(input);
+
+  std::vector<size_t> send_lengths(size);
+  std::vector<size_t> send_offsets(size);
+  std::vector<size_t> recv_lengths(size);
+  std::vector<size_t> recv_offsets(size);
+  compute_lengths_and_offsets(
+      input_split_sizes, input, &send_lengths, &send_offsets);
+  compute_lengths_and_offsets(
+      output_split_sizes, output, &recv_lengths, &recv_offsets);
+
+  const char* sendbuff = reinterpret_cast<const char*>(input.const_data_ptr());
+  char* recvbuff = reinterpret_cast<char*>(output.data_ptr());
+
+  cudaStream_t stream = at::cuda::getCurrentCUDAStream().stream();
+
+  NCCL_CHECK(ncclGroupStart());
+  for (int r = 0; r < size; ++r) {
+    NCCL_CHECK(ncclSend(/*sendbuff=*/sendbuff + (send_offsets[r] * ele_size),
+                        /*count=*/send_lengths[r],
+                        /*datatype=*/type,
+                        /*peer=*/r,
+                        /*comm=*/comm_,
+                        /*stream=*/stream));
+    NCCL_CHECK(ncclRecv(/*recvbuff=*/recvbuff + (recv_offsets[r] * ele_size),
+                        /*count=*/recv_lengths[r],
+                        /*datatype=*/type,
+                        /*peer=*/r,
+                        /*comm=*/comm_,
+                        /*stream=*/stream));
+  }
+  NCCL_CHECK(ncclGroupEnd());
 }
 
 }  // namespace llm
--- src/model_parallel/process_group_test.cpp
+++ src/model_parallel/process_group_test.cpp
@@ -1,40 +1,29 @@
 #include "process_group.h"
 
-#include <c10/core/Device.h>
 #include <c10/cuda/CUDAStream.h>
 #include <gtest/gtest.h>
-#include <torch/cuda.h>
+#include <torch/types.h>
 
 namespace llm {
-
-void run_collective_test(
-    int world_size,
-    std::function<void(const std::vector<torch::Tensor>& tensors,
-                       ProcessGroup* pg)> func) {
+namespace {
+void run_collective_test(int world_size,
+                         torch::DeviceType device_type,
+                         std::function<void(ProcessGroup* pg)> func) {
   // create process groups
   std::vector<torch::Device> devices;
   devices.reserve(world_size);
   for (int i = 0; i < world_size; ++i) {
-    devices.emplace_back(torch::kCUDA, i);
+    devices.emplace_back(device_type, i);
   }
   auto process_groups = ProcessGroup::create_process_groups(devices);
   EXPECT_EQ(process_groups.size(), world_size);
 
-  // create tensors
-  const int num_test_tensors = 50;
-  std::vector<torch::Tensor> tensors;
-  tensors.reserve(num_test_tensors);
-  for (int i = 0; i < num_test_tensors; ++i) {
-    tensors.push_back(torch::ones({100, 4096}, torch::kHalf));
-  }
-
-  // run all reduce
+  // run collective test in parallel
   std::vector<std::thread> threads;
   threads.reserve(process_groups.size());
   for (int i = 0; i < world_size; ++i) {
-    threads.emplace_back([func, &tensors, pg = process_groups[i].get()]() {
-      func(tensors, pg);
-    });
+    ProcessGroup* pg = process_groups[i].get();
+    threads.emplace_back([func, pg]() { func(pg); });
   }
 
   // wait for all threads to finish
@@ -45,62 +34,138 @@ void run_collective_test(
   }
 }
 
-TEST(ProcessGroupTest, NCCLAllReduce) {
-  // skip test if less than two gpus
-  if (torch::cuda::device_count() < 2) {
-    GTEST_SKIP() << "Skipping test because less than two gpus";
-  }
+template <typename T>
+std::vector<T> to_vector(const torch::Tensor& tensor) {
+  auto t = tensor.cpu();
+  return {t.const_data_ptr<T>(), t.const_data_ptr<T>() + t.numel()};
+}
+}  // namespace
 
-  for (int i = 2; i <= torch::cuda::device_count(); i *= 2) {
-    run_collective_test(
-        i, [](const std::vector<torch::Tensor>& tensors, ProcessGroup* pg) {
-          const int rank = pg->rank();
-          const int world_size = pg->world_size();
-          const auto& device = pg->device();
-          torch::DeviceGuard device_guard(device);
-          at::cuda::CUDAStream stream = at::cuda::getCurrentCUDAStream();
-          for (int i = 0; i <= tensors.size() - world_size; ++i) {
-            auto tensor = tensors[i + rank].to(device);
-            pg->allreduce(tensor);
-            stream.synchronize();
-            auto expected = torch::zeros_like(tensors[i]);
-            for (int j = 0; j < world_size; ++j) {
-              expected += tensors[i + j];
-            }
-            EXPECT_TRUE(torch::equal(tensor.cpu(), expected));
-          }
-        });
+class CollectiveTest : public ::testing::TestWithParam<
+                           std::tuple<torch::DeviceType, torch::ScalarType>> {};
+
+TEST_P(CollectiveTest, AllReduce) {
+  const auto& [device_type, dtype] = GetParam();
+
+  // [1, 2, 4, 8]
+  for (int world_size = 1; world_size <= torch::cuda::device_count();
+       world_size *= 2) {
+    // create tensors
+    const int num_test_tensors = 50;
+    std::vector<torch::Tensor> tensors;
+    tensors.reserve(num_test_tensors);
+    for (int i = 0; i < num_test_tensors; ++i) {
+      tensors.push_back(torch::randn({100, 4096}, dtype));
+    }
+
+    run_collective_test(world_size, device_type, [&tensors](ProcessGroup* pg) {
+      const int rank = pg->rank();
+      const int size = pg->world_size();
+      const auto& device = pg->device();
+      torch::DeviceGuard device_guard(device);
+      at::cuda::CUDAStream stream = at::cuda::getCurrentCUDAStream();
+      for (int i = 0; i <= tensors.size() - size; ++i) {
+        auto tensor = tensors[i + rank].to(device);
+        pg->allreduce(tensor);
+        stream.synchronize();
+        // check the result
+        auto expected = torch::zeros_like(tensors[i]);
+        for (int j = 0; j < size; ++j) {
+          expected += tensors[i + j];
+        }
+        EXPECT_TRUE(torch::equal(tensor.cpu(), expected));
+      }
+    });
   }
 }
 
-TEST(ProcessGroupTest, NCCLAllGather) {
-  // skip test if less than two gpus
-  if (torch::cuda::device_count() < 2) {
-    GTEST_SKIP() << "Skipping test because less than two gpus";
-  }
+TEST_P(CollectiveTest, AllGather) {
+  const auto& [device_type, dtype] = GetParam();
 
-  for (int i = 2; i <= torch::cuda::device_count(); i *= 2) {
-    run_collective_test(
-        i, [](const std::vector<torch::Tensor>& tensors, ProcessGroup* pg) {
-          const int rank = pg->rank();
-          const int world_size = pg->world_size();
-          const auto& device = pg->device();
-          torch::DeviceGuard device_guard(device);
-          at::cuda::CUDAStream stream = at::cuda::getCurrentCUDAStream();
-          for (int i = 0; i <= tensors.size() - world_size; ++i) {
-            auto tensor = tensors[i + rank].to(device);
-            std::vector<torch::Tensor> outputs(world_size);
-            for (int j = 0; j < world_size; ++j) {
-              outputs[j] = torch::empty_like(tensor);
-            }
-            pg->allgather(tensor, outputs);
-            stream.synchronize();
-            for (int j = 0; j < world_size; ++j) {
-              EXPECT_TRUE(torch::equal(tensors[i + j], outputs[j].cpu()));
-            }
-          }
-        });
+  for (int world_size = 1; world_size <= torch::cuda::device_count();
+       world_size *= 2) {
+    // create tensors
+    const int num_test_tensors = 50;
+    std::vector<torch::Tensor> tensors;
+    tensors.reserve(num_test_tensors);
+    for (int i = 0; i < num_test_tensors; ++i) {
+      tensors.push_back(torch::ones({100, 4096}, dtype));
+    }
+
+    run_collective_test(world_size, device_type, [&tensors](ProcessGroup* pg) {
+      const int rank = pg->rank();
+      const int size = pg->world_size();
+      const auto& device = pg->device();
+      torch::DeviceGuard device_guard(device);
+      at::cuda::CUDAStream stream = at::cuda::getCurrentCUDAStream();
+      for (int i = 0; i <= tensors.size() - size; ++i) {
+        auto tensor = tensors[i + rank].to(device);
+        std::vector<torch::Tensor> outputs(size);
+        for (int j = 0; j < size; ++j) {
+          outputs[j] = torch::empty_like(tensor);
+        }
+        pg->allgather(tensor, outputs);
+        stream.synchronize();
+        for (int j = 0; j < size; ++j) {
+          EXPECT_TRUE(torch::equal(tensors[i + j], outputs[j].cpu()));
+        }
+      }
+    });
   }
 }
+
+TEST_P(CollectiveTest, AllToAll) {
+  const auto& [device_type, dtype] = GetParam();
+
+  for (int world_size = 1; world_size <= torch::cuda::device_count();
+       world_size *= 2) {
+    run_collective_test(world_size, device_type, [dtype](ProcessGroup* pg) {
+      const int size = pg->world_size();
+      const auto& device = pg->device();
+      torch::DeviceGuard device_guard(device);
+      at::cuda::CUDAStream stream = at::cuda::getCurrentCUDAStream();
+
+      // create size tensors
+      auto size_options = torch::device(device).dtype(torch::kInt64);
+      auto input_splits = torch::randint(1, 100, {size}, size_options);
+      auto output_splits = torch::empty({size}, size_options);
+      // alltoall communication for sizes
+      pg->alltoall(input_splits, output_splits);
+
+      // sizes to vector
+      auto input_split_sizes = to_vector<int64_t>(input_splits);
+      auto output_split_sizes = to_vector<int64_t>(output_splits);
+
+      const auto input_size =
+          std::reduce(input_split_sizes.begin(), input_split_sizes.end());
+      const auto output_size =
+          std::reduce(output_split_sizes.begin(), output_split_sizes.end());
+
+      // create tensors
+      auto options = torch::device(device).dtype(dtype);
+      auto input = torch::randn({input_size}, options);
+      auto output = torch::empty({output_size}, options);
+
+      // alltoall communication for data
+      pg->alltoall(input, output, input_split_sizes, output_split_sizes);
+
+      auto input2 = torch::empty_like(input);
+      // alltoall communication again with swapped input and output
+      // NOLINTNEXTLINE(readability-suspicious-call-argument)
+      pg->alltoall(output, input2, output_split_sizes, input_split_sizes);
+
+      // we should get the same input tensor back
+      EXPECT_TRUE(torch::equal(input, input2));
+    });
+  }
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    ProcessGroupTest,
+    CollectiveTest,
+    ::testing::Combine(::testing::Values(torch::kCUDA),  // device type
+                       ::testing::Values(torch::kHalf,
+                                         torch::kBFloat16)  // dtype
+                       ));
 
 }  // namespace llm
