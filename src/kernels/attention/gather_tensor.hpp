9d1430fda653cd6c2bd6f26b8344dfc7a74736bd
Michael Mi <guocuimi@gmail.com>
2025-04-14 17:36:41 -0700

    add comments

--- src/kernels/attention/gather_tensor.hpp
+++ src/kernels/attention/gather_tensor.hpp
@@ -161,6 +161,8 @@ CUTE_HOST_DEVICE constexpr auto max_common_vector(
     ComposedLayout<Layout<OuterShapeB, OuterStrideB>,
                    OffsetB,
                    Layout<ShapeB, StrideB>> const& b) {
+  // we know it is safe to disable auto-vectorization for gather tensors
+  // since we are using vectorized asynchronous copy.
   return Int<1>{};
 }
 
