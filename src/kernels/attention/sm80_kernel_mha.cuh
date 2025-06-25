82c5ca58fcf5c74e96eaf16cc0316d11c990cfc3
Michael Mi <guocuimi@gmail.com>
2025-06-24 22:20:15 -0700

    move tile handling into kernel

--- src/kernels/attention/sm80_kernel_mha.cuh
+++ src/kernels/attention/sm80_kernel_mha.cuh
@@ -6,12 +6,161 @@
 #include <cute/layout.hpp>
 #include <cute/tensor.hpp>
 
-#include "mha_tile.h"
+#include "gather_tensor.hpp"
+#include "mha_params.h"
 #include "online_softmax.cuh"
 
 namespace llm {
 
 using namespace cute;
+
+namespace detail {
+
+template <typename Params>
+struct MHATile {
+  static_assert(cute::dependent_false<Params>, "not implemented");
+};
+
+// AttentionTile specialization for AttentionParams
+template <>
+struct MHATile<MHAParams> {
+  const MHAParams& params_;
+  const int batch_idx_;
+  const int kv_head_idx_;
+
+  CUTE_HOST_DEVICE MHATile(const MHAParams& params,
+                           int batch_idx,
+                           int kv_head_idx)
+      : params_(params), batch_idx_(batch_idx), kv_head_idx_(kv_head_idx) {}
+
+  // return the query/output tile: (q_len, head_dim)
+  template <typename Element>
+  CUTE_HOST_DEVICE auto get_qo_tile() const {
+    // (batch, seq, head, dim)
+
+    // packed all q/o in the same kv head group together
+    const auto head_base = kv_head_idx_ * params_.group_size;
+    auto packed_idx_to_coord = [this, head_base](int packed_idx) {
+      int idx, offset;
+      params_.group_size.divmod(packed_idx, idx, offset);
+      return make_coord(idx, head_base + offset);
+    };
+
+    const auto packed_len = params_.q_len * params_.group_size;
+    const auto q_offset = batch_idx_ * get<0>(params_.q_stride);
+    auto q = make_gather_tensor(
+        make_gmem_ptr((const Element*)params_.q_ptr + q_offset),
+        make_shape(packed_len, params_.head_dim),
+        make_stride(select<1, 2>(params_.q_stride), get<3>(params_.q_stride)),
+        packed_idx_to_coord);
+
+    const auto o_offset = batch_idx_ * get<0>(params_.o_stride);
+    auto o = make_gather_tensor(
+        make_gmem_ptr((Element*)params_.o_ptr + o_offset),
+        make_shape(packed_len, params_.head_dim),
+        make_stride(select<1, 2>(params_.o_stride), get<3>(params_.o_stride)),
+        packed_idx_to_coord);
+    return make_tuple(q, o);
+  }
+
+  // return the key/value tile: (kv_len, head_dim)
+  template <typename Element>
+  CUTE_HOST_DEVICE auto get_kv_tile() const {
+    // (batch, seq, kv_head, dim)
+    const auto k_offset = batch_idx_ * get<0>(params_.k_stride) +
+                          kv_head_idx_ * get<2>(params_.k_stride);
+    const auto v_offset = batch_idx_ * get<0>(params_.v_stride) +
+                          kv_head_idx_ * get<2>(params_.v_stride);
+    // k[batch_idx, :, kv_head_idx, :]
+    auto k =
+        make_tensor(make_gmem_ptr((const Element*)params_.k_ptr + k_offset),
+                    make_shape(params_.kv_len, params_.head_dim),
+                    select<1, 3>(params_.k_stride));
+    // v[batch_idx, :, kv_head_idx, :]
+    auto v =
+        make_tensor(make_gmem_ptr((const Element*)params_.v_ptr + v_offset),
+                    make_shape(params_.kv_len, params_.head_dim),
+                    select<1, 3>(params_.v_stride));
+    return make_tuple(k, v);
+  }
+};
+
+// paged KV cache + variable length sequence
+template <>
+struct MHATile<MHAPagedKVParams> {
+  // NOLINTNEXTLINE
+  const MHAPagedKVParams& params_;
+  const int batch_idx_;
+  const int kv_head_idx_;
+
+  CUTE_HOST_DEVICE MHATile(const MHAPagedKVParams& params,
+                           int batch_idx,
+                           int kv_head_idx)
+      : params_(params), batch_idx_(batch_idx), kv_head_idx_(kv_head_idx) {}
+
+  // return the query/output tile: (q_len, head_dim)
+  template <typename Element>
+  CUTE_HOST_DEVICE auto get_qo_tile() const {
+    const auto begin = params_.q_cu_lens[batch_idx_];
+    const auto qo_len = params_.q_cu_lens[batch_idx_ + 1] - begin;
+    const auto head_base = kv_head_idx_ * params_.group_size;
+    auto packed_idx_to_coord = [this, head_base](int packed_idx) {
+      int idx, offset;
+      params_.group_size.divmod(packed_idx, idx, offset);
+      return make_coord(idx, head_base + offset);
+    };
+
+    const auto packed_len = qo_len * params_.group_size;
+    const auto q_offset = begin * get<0>(params_.q_stride);
+    auto q = make_gather_tensor(
+        make_gmem_ptr((const Element*)params_.q_ptr + q_offset),
+        make_shape(packed_len, params_.head_dim),
+        make_stride(select<0, 1>(params_.q_stride), get<2>(params_.q_stride)),
+        packed_idx_to_coord);
+
+    const auto o_offset = begin * get<0>(params_.o_stride);
+    auto o = make_gather_tensor(
+        make_gmem_ptr((Element*)params_.o_ptr + o_offset),
+        make_shape(packed_len, params_.head_dim),
+        make_stride(select<0, 1>(params_.o_stride), get<2>(params_.o_stride)),
+        packed_idx_to_coord);
+    return make_tuple(q, o);
+  }
+
+  // return the key/value tile: (kv_len, head_dim)
+  template <typename Element>
+  CUTE_HOST_DEVICE auto get_kv_tile() const {
+    const auto kv_len =
+        params_.kv_cu_lens[batch_idx_ + 1] - params_.kv_cu_lens[batch_idx_];
+
+    // map seq_idx to slot_idx
+    const int* block_table =
+        params_.block_table + params_.block_cu_lens[batch_idx_];
+    auto idx_to_slot = [block_table,
+                        shr = params_.block_shift_right,
+                        mask = params_.block_mask](int idx) {
+      return block_table[idx >> shr] + (idx & mask);
+    };
+
+    // v[:, kv_head_idx, :]
+    const auto k_offset = kv_head_idx_ * get<1>(params_.k_stride);
+    auto k = make_gather_tensor(
+        make_gmem_ptr((const Element*)params_.k_ptr + k_offset),
+        make_shape(kv_len, params_.head_dim),
+        select<0, 2>(params_.k_stride),
+        idx_to_slot);
+
+    // v[:, kv_head_idx, :]
+    const auto v_offset = kv_head_idx_ * get<1>(params_.v_stride);
+    auto v = make_gather_tensor(
+        make_gmem_ptr((const Element*)params_.v_ptr + v_offset),
+        make_shape(kv_len, params_.head_dim),
+        select<0, 2>(params_.v_stride),
+        idx_to_slot);
+    return make_tuple(k, v);
+  }
+};
+}  // namespace detail
 
 template <class CollectiveMainloop_,
           class CollectiveEpilogue_,
@@ -75,7 +224,7 @@ class Sm80KernelMha {
       const auto tidx = threadIdx.x;
 
       // (q_packed_len, HEAD_DIM)
-      MHATile<Params> tile(params, batch_idx, kv_head_idx);
+      detail::MHATile<Params> tile(params, batch_idx, kv_head_idx);
       auto [Q, O] = tile.template get_qo_tile<Element>();
       // (kv_len, HEAD_DIM)
       auto [K, V] = tile.template get_kv_tile<Element>();
--- src/kernels/attention/sm80_kernel_mla.cuh
+++ src/kernels/attention/sm80_kernel_mla.cuh
@@ -6,12 +6,152 @@
 #include <cute/layout.hpp>
 #include <cute/tensor.hpp>
 
-#include "mla_tile.h"
+#include "gather_tensor.hpp"
+#include "mla_params.h"
 #include "online_softmax.cuh"
 
 namespace llm {
 
 using namespace cute;
+
+namespace detail {
+
+template <typename Params>
+struct MLATile {
+  static_assert(cute::dependent_false<Params>, "not implemented");
+};
+
+// AttentionTile specialization for AttentionParams
+template <>
+struct MLATile<MLAParams> {
+  const MLAParams& params_;
+  const int batch_idx_;
+
+  CUTE_HOST_DEVICE MLATile(const MLAParams& params, int batch_idx)
+      : params_(params), batch_idx_(batch_idx) {}
+
+  // return the query/output tile: (q_packed_len, head_dim)
+  // return q_rope tile: (q_packed_len, rope_head_dim)
+  template <typename Element>
+  CUTE_HOST_DEVICE auto get_qo_tile() const {
+    // (batch, seq, head, dim)
+    const auto q_packed_len = params_.q_len * params_.group_size;
+    const auto q_offset = batch_idx_ * get<0>(params_.q_stride);
+    auto q =
+        make_tensor(make_gmem_ptr((const Element*)params_.q_ptr + q_offset),
+                    make_shape(q_packed_len, params_.head_dim),
+                    select<2, 3>(params_.q_stride));
+
+    // (batch, seq, head, rope_head_dim)
+    const auto q_rope_offset = batch_idx_ * get<0>(params_.q_rope_stride);
+    auto q_rope = make_tensor(
+        make_gmem_ptr((const Element*)params_.q_rope_ptr + q_rope_offset),
+        make_shape(q_packed_len, params_.rope_head_dim),
+        select<2, 3>(params_.q_rope_stride));
+
+    // (batch, seq, head, dim)
+    const auto o_offset = batch_idx_ * get<0>(params_.o_stride);
+    auto o = make_tensor(make_gmem_ptr((Element*)params_.o_ptr + o_offset),
+                         make_shape(q_packed_len, params_.head_dim),
+                         select<2, 3>(params_.o_stride));
+    return make_tuple(q, q_rope, o);
+  }
+
+  // return the kv: (kv_len, head_dim)
+  // return k_rope: (kv_len, rope_head_dim)
+  template <typename Element>
+  CUTE_HOST_DEVICE auto get_kv_tile() const {
+    // (batch, seq, dim)
+    const auto kv_offset = batch_idx_ * get<0>(params_.kv_stride);
+    // k[batch_idx, :, :]
+    auto kv =
+        make_tensor(make_gmem_ptr((const Element*)params_.kv_ptr + kv_offset),
+                    make_shape(params_.kv_len, params_.head_dim),
+                    select<1, 2>(params_.kv_stride));
+
+    // (batch, seq, rope_head_dim)
+    const auto k_rope_offset = batch_idx_ * get<0>(params_.k_rope_stride);
+    auto k_rope = make_tensor(
+        make_gmem_ptr((const Element*)params_.k_rope_ptr + k_rope_offset),
+        make_shape(params_.kv_len, params_.rope_head_dim),
+        select<1, 2>(params_.k_rope_stride));
+    return make_tuple(kv, k_rope);
+  }
+};
+
+// paged KV cache + variable length sequence
+template <>
+struct MLATile<MLAPagedKVParams> {
+  // NOLINTNEXTLINE
+  const MLAPagedKVParams& params_;
+  const int batch_idx_;
+
+  CUTE_HOST_DEVICE MLATile(const MLAPagedKVParams& params, int batch_idx)
+      : params_(params), batch_idx_(batch_idx) {}
+
+  // return the query/output tile: (q_packed_len, head_dim)
+  // return q_rope tile: (q_packed_len, rope_head_dim)
+  template <typename Element>
+  CUTE_HOST_DEVICE auto get_qo_tile() const {
+    const auto begin = params_.q_cu_lens[batch_idx_];
+    const auto qo_len = params_.q_cu_lens[batch_idx_ + 1] - begin;
+
+    // (seq, head, dim)
+    const auto q_packed_len = qo_len * params_.group_size;
+    const auto q_offset = begin * get<0>(params_.q_stride);
+
+    auto q =
+        make_tensor(make_gmem_ptr((const Element*)params_.q_ptr + q_offset),
+                    make_shape(q_packed_len, params_.head_dim),
+                    select<1, 2>(params_.q_stride));
+
+    // (seq, head, rope_head_dim)
+    const auto q_rope_offset = begin * get<0>(params_.q_rope_stride);
+    auto q_rope = make_tensor(
+        make_gmem_ptr((const Element*)params_.q_rope_ptr + q_rope_offset),
+        make_shape(q_packed_len, params_.rope_head_dim),
+        select<1, 2>(params_.q_rope_stride));
+
+    // (seq, head, dim)
+    const auto o_offset = begin * get<0>(params_.o_stride);
+    auto o = make_tensor(make_gmem_ptr((Element*)params_.o_ptr + o_offset),
+                         make_shape(q_packed_len, params_.head_dim),
+                         select<1, 2>(params_.o_stride));
+    return make_tuple(q, q_rope, o);
+  }
+
+  // return the kv: (kv_len, head_dim)
+  // return k_rope: (kv_len, rope_head_dim)
+  template <typename Element>
+  CUTE_HOST_DEVICE auto get_kv_tile() const {
+    const auto kv_len =
+        params_.kv_cu_lens[batch_idx_ + 1] - params_.kv_cu_lens[batch_idx_];
+
+    // map seq_idx to slot_idx
+    const int* block_table =
+        params_.block_table + params_.block_cu_lens[batch_idx_];
+    auto idx_to_slot = [block_table,
+                        shr = params_.block_shift_right,
+                        mask = params_.block_mask](int idx) {
+      return block_table[idx >> shr] + (idx & mask);
+    };
+
+    // kv: (seq, dim)
+    auto kv = make_gather_tensor(make_gmem_ptr((const Element*)params_.kv_ptr),
+                                 make_shape(kv_len, params_.head_dim),
+                                 params_.kv_stride,
+                                 idx_to_slot);
+
+    // k_rope: (seq, rope_head_dim)
+    auto k_rope =
+        make_gather_tensor(make_gmem_ptr((const Element*)params_.k_rope_ptr),
+                           make_shape(kv_len, params_.rope_head_dim),
+                           params_.k_rope_stride,
+                           idx_to_slot);
+    return make_tuple(kv, k_rope);
+  }
+};
+}  // namespace detail
 
 template <class CollectiveMainloop_,
           class CollectiveEpilogue_,
@@ -76,7 +216,7 @@ class Sm80KernelMla {
 
       // Q/O: (q_packed_len, HEAD_DIM)
       // Q_ROPE: (q_packed_len, ROPE_HEAD_DIM)
-      MLATile<Params> tile(params, batch_idx);
+      detail::MLATile<Params> tile(params, batch_idx);
       auto [Q, Q_ROPE, O] = tile.template get_qo_tile<Element>();
       // KV: (kv_len, HEAD_DIM)
       // K_ROPE: (kv_len, ROPE_HEAD_DIM)
