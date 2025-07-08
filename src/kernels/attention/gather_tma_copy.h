0d6f9b9305d003e4eee6ca7ba8014d554e142d88
Michael Mi <guocuimi@gmail.com>
2025-07-07 18:35:17 -0700

    clean up

--- src/kernels/attention/gather_tma_copy.h
+++ src/kernels/attention/gather_tma_copy.h
@@ -1,18 +1,5 @@
 #pragma once
-
-// #if !defined(__CUDACC_RTC__)
-// #include <cuda.h>
-// #endif
-
-// #include <cute/algorithm/prefetch.hpp>
-// #include <cute/atom/copy_atom.hpp>
-// #include <cute/atom/copy_traits.hpp>
 #include <cute/atom/copy_traits_sm90_tma.hpp>
-
-#include "cute/swizzle_layout.hpp"
-// #include <cute/atom/copy_traits_sm90_tma_swizzle.hpp>
-// #include <cute/numeric/integral_ratio.hpp>
-// #include <cutlass/cuda_host_adapter.hpp>
 
 namespace cute {
 
@@ -39,27 +26,13 @@ CUTE_HOST_RTC auto make_gather_tma_copy_atom(
     SLayout const& slayout,         // CTA Tile of SMEM, potentially swizzled
     uint32_t const& num_multicast)  // The num of CTAs involved in multicasting)
 {
-  auto slayout_atom = get_tma_atom_slayout(slayout);
-  auto atom_tiler = product_each(shape(slayout_atom));
+  auto atom_slayout = get_tma_atom_slayout(slayout);
+  auto atom_tiler = product_each(shape(atom_slayout));
   auto atom_v_map = make_identity_layout(shape(gtensor)).compose(atom_tiler);
-  print("slayout_atom: ");
-  print(slayout_atom);
-  print("\n");
-  print("atom_tiler: ");
-  print(atom_tiler);
-  print("\n");
-  print("atom_v_map: ");
-  print(atom_v_map);
-  print("\n");
-
   return make_tma_copy_atom<TmaInternalType>(
-      copy_op, gtensor, slayout_atom, num_multicast, atom_v_map);
+      copy_op, gtensor, atom_slayout, num_multicast, atom_v_map);
 }
 
-// The "logical TMA tid" is a map from the CTA rank to its logical id
-// within the instruction.  It works like a mask or ordering on the
-// CTAs.  For non-multicast TMA, all CTAs should map to 0.  For
-// multicast TMA of size 4, CTAs will be mapped to {0,1,2,3}.
 template <class TmaType,
           class CopyOp,
           class GEngine,
@@ -101,23 +74,11 @@ CUTE_HOST_RTC auto make_gather_tma_copy_tiled(
       composition(inv_smem_layout, composition(layout_t, cta_t_map));
   // Combine with the T mapping
   [[maybe_unused]] auto layout_TV = make_layout(layout_T, layout_V);
-
-  // clang-format off
-#if 0
-  print("cta_tiler : "); print(cta_tiler); print("\n");
-  print("layout_v : "); print(layout_v); print("\n");
-  print("layout_V : "); print(layout_V); print("\n");
-  print("layout_t : "); print(layout_t); print("\n");
-  print("layout_T : "); print(layout_T); print("\n");
-  print("layout_TV : "); print(layout_TV); print("\n");
-#endif
-  // clang-format on
-
   return TiledCopy<decltype(atom), decltype(layout_TV), decltype(cta_tiler)>{
       atom};
 }
 
-}  // end namespace detail
+}  // namespace detail
 
 template <class TmaInternalType = void,
           class CopyOp,
@@ -136,11 +97,10 @@ CUTE_HOST_RTC auto make_gather_tma_copy(
   auto cta_t_map = make_layout(cluster_size);
   // CTA val idx -> gmem mode
   auto cta_v_map = make_identity_layout(shape(gtensor)).compose(cta_tiler);
-  // Prefer TmaInternalType if specified. Fallback to GEngine::value_type
   using TmaType = conditional_t<is_same_v<void, TmaInternalType>,
                                 typename GEngine::value_type,
                                 TmaInternalType>;
   return detail::make_gather_tma_copy_tiled<TmaType>(
       copy_op, gtensor, slayout, cta_t_map, cta_v_map);
 }
-}  // end namespace cute
+}  // namespace cute
